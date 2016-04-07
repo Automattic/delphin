96f79bf661ba0047e1983b7d81d26dae7c9b4b13
Ben Dwyer <ben@scruffian.com>
2016-04-07 14:35:14 +0100

    Server: Fixing linting issues

--- server/index.js
+++ server/index.js
@@ -1,36 +1,35 @@
-
 var express = require( 'express' ),
 	path = require( 'path' ),
 	webpack = require( 'webpack' ),
-	app = express();
+	app = express(),
+	isDevelopment, config, WebpackDevServer, compiler, devServer;
 
 app.set( 'port', process.env.PORT || 1337 );
 
-app.use( '/build', express.static( path.join(__dirname, '..', 'build') ) );
+app.use( '/build', express.static( path.join( __dirname, '..', 'build' ) ) );
 
 app.get( '/*', function( req, res ) {
 	res.sendFile( 'index.html', { root: path.join( __dirname, 'views' ) } );
-});
+} );
 
-var isDevelopment = 'production' !== process.env.NODE_ENV;
+isDevelopment = 'production' !== process.env.NODE_ENV;
 if ( isDevelopment ) {
 	app.set( 'backend-port', app.get( 'port' ) + 1 );
 
-	var config = require( '../webpack.config' );
+	config = require( '../webpack.config' );
 	config.entry.bundle.unshift( 'webpack/hot/only-dev-server' );
 	config.entry.bundle.unshift( 'webpack-dev-server/client?/' );
 	config.plugins.push( new webpack.HotModuleReplacementPlugin() );
 
 	config.module.loaders.unshift( {
-		test:   /\.jsx?$/,
+		test: /\.jsx?$/,
 		loader: 'react-hot',
 		include: path.join( __dirname, '../client' )
 	} );
-	console.log( config.module.loaders );
 
-	var WebpackDevServer = require( 'webpack-dev-server' );
-	var compiler = webpack( config );
-	var devServer = new WebpackDevServer( compiler, {
+	WebpackDevServer = require( 'webpack-dev-server' );
+	compiler = webpack( config );
+	devServer = new WebpackDevServer( compiler, {
 		publicPath: config.output.publicPath,
 		hot: true,
 		proxy: {
@@ -45,7 +44,6 @@ if ( isDevelopment ) {
 	app.listen( app.get( 'backend-port' ), function( err ) {
 		console.log( err || 'Backend listening on http://localhost:' + app.get( 'backend-port' ) );
 	} );
-
 } else {
 	app.listen( app.get( 'port' ), function( err ) {
 		console.log( err || 'Server listening on http://localhost:' + app.get( 'port' ) );

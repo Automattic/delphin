8208b687e4d77b85b1bf42677719f6a690f25189
Ben Dwyer <ben@scruffian.com>
2016-04-12 14:59:20 +0100

    Removing duplicate CSS

--- client/components/root.js
+++ client/components/root.js
@@ -1,25 +1,5 @@
 import React from 'react';
 import { Link } from 'react-router';
-import Search from './search';
-
-const CSS = {
-	body: {
-		fontFamily: 'Helvetica, Arial, sans-serif'
-	},
-	header: {
-		background: '#000',
-	},
-	headerLink: {
-		color: '#fff',
-		display: 'inline-block',
-		margin: '0.5em 1em',
-		textDecoration: 'none'
-	},
-	main: {
-		margin: '0 auto',
-		maxWidth: '960px',
-	}
-};
 
 const CSS = {
 	body: {

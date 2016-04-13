39e4174f5acf7738cf48c9459a098facc2d28034
Stéphane Thomas <email@stephanethomas.com>
2016-04-13 12:56:57 +0100

    Format domain search reducer to improve readability

--- app/reducers/domain-search/index.js
+++ app/reducers/domain-search/index.js
@@ -4,16 +4,20 @@ const initialState = {
 };
 
 export function domainSearch( state = initialState, action ) {
-	const { type, results } = action;
+	const { results, type } = action;
 
 	switch ( type ) {
 		case 'DOMAIN_SEARCH_FETCH':
-			return Object.assign( {}, state, { isFetching: true } );
+			return Object.assign( {}, state, {
+				isFetching: true
+			} );
+
 		case 'DOMAIN_SEARCH_FETCH_COMPLETED':
 			return Object.assign( {}, state, {
 				isFetching: false,
 				results: results
 			} );
+
 		default:
 			return state;
 	}

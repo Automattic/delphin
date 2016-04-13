ba33c289713c54bdb674101c7c1f0c94de1fe8e7
Stéphane Thomas <email@stephanethomas.com>
2016-04-13 12:44:25 +0100

    Reorder checkout reducer actions alphabetically

--- app/reducers/checkout/index.js
+++ app/reducers/checkout/index.js
@@ -2,19 +2,6 @@ export function checkout( state = {}, action ) {
 	const { domain, type } = action;
 
 	switch ( type ) {
-		case 'SELECT_DOMAIN':
-			return { domain };
-
-		case 'CREATE_USER_COMPLETE':
-			return Object.assign( {}, state, {
-				user: {
-					username: action.username,
-					email: action.email,
-					password: action.password,
-					bearerToken: action.bearerToken
-				}
-			} );
-
 		case 'CREATE_SITE_COMPLETE':
 			return Object.assign( {}, state, {
 				site: {
@@ -27,6 +14,19 @@ export function checkout( state = {}, action ) {
 			return Object.assign( {}, state, {
 				transaction: action.form
 			} );
+
+		case 'CREATE_USER_COMPLETE':
+			return Object.assign( {}, state, {
+				user: {
+					username: action.username,
+					email: action.email,
+					password: action.password,
+					bearerToken: action.bearerToken
+				}
+			} );
+
+		case 'SELECT_DOMAIN':
+			return { domain };
 
 		default:
 			return state;

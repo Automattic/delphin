6c1d7174d3d7245ccb5040450474886010aaa7a8
Drew Blaisdell <drew.blaisdell@gmail.com>
2016-04-14 15:43:32 +0100

    Use ES6 syntax in `server/index`

--- server/index.js
+++ server/index.js
@@ -1,32 +1,29 @@
 // External dependencies
-var express = require( 'express' ),
-	app = express(),
-	path = require( 'path' ),
-	webpack = require( 'webpack' ),
-	isDevelopment = 'production' !== process.env.NODE_ENV,
+import express from 'express';
+import path from 'path';
+import webpack from 'webpack';
+import pug from 'pug';
+import fs from 'fs';
+import WebpackDevServer from 'webpack-dev-server';
+import config from '../webpack.config';
+
+const app = express(),
 	port = process.env.PORT || 1337,
-	pug = require( 'pug' ),
-	fs = require( 'fs' ),
-	backendPort, isDevelopment, config, WebpackDevServer, compiler, devServer, template, templateCompiler, templatePath;
+	templatePath = path.join( __dirname, 'views', 'index.pug' ),
+	template = fs.readFileSync( templatePath, 'utf8' ),
+	templateCompiler = pug.compile( template, { filename: templatePath, pretty: true } );
 
 app.use( '/build', express.static( path.join( __dirname, '..', 'build' ) ) );
 
-app.get( '/*', function( req, res ) {
-	templatePath = path.join( __dirname, 'views', 'index.pug' );
-	template = fs.readFileSync( templatePath, 'utf8' );
-	templateCompiler = pug.compile( template, { filename: templatePath, pretty: true } );
-
+app.get( '/*', ( req, res ) => {
 	res.send( templateCompiler() );
 } );
 
+const isDevelopment = 'production' !== process.env.NODE_ENV;
 if ( isDevelopment ) {
-	backendPort = port + 1;
+	const backendPort = port + 1;
 
-	WebpackDevServer = require( 'webpack-dev-server' );
-	config = require( '../webpack.config' );
-	compiler = webpack( config );
-
-	devServer = new WebpackDevServer( compiler, {
+	const devServer = new WebpackDevServer( webpack( config ), {
 		publicPath: config.output.publicPath,
 		hot: true,
 		proxy: {
@@ -35,14 +32,14 @@ if ( isDevelopment ) {
 		stats: { colors: true }
 	} );
 
-	devServer.listen( port, function( err ) {
-		console.log( err || 'Server listening on http://localhost:' + port );
+	devServer.listen( port, error => {
+		console.log( error || 'Server listening on http://localhost:' + port );
 	} );
-	app.listen( backendPort, 'localhost', function( err ) {
-		console.log( err || 'Backend listening on http://localhost:' + backendPort );
+	app.listen( backendPort, 'localhost', error => {
+		console.log( error || 'Backend listening on http://localhost:' + backendPort );
 	} );
 } else {
-	app.listen( port, function( err ) {
-		console.log( err || 'Server listening on http://localhost:' + port );
+	app.listen( port, error => {
+		console.log( error || 'Server listening on http://localhost:' + port );
 	} );
 }

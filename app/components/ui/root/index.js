67118d7c4a37e2307884b45d2e5158eb49ef969d
Ben Dwyer <ben@scruffian.com>
2016-04-15 15:16:38 +0100

    Linting fix

--- app/components/ui/root/index.js
+++ app/components/ui/root/index.js
@@ -15,9 +15,9 @@ const CSS = {
 		boxShadow: '-1px -1px 1px rgba( 0, 0, 0, .2 )',
 		fontSize: '0.8rem',
 		position: 'fixed',
-			bottom: 0,
-			left: 0,
-			right: 0,
+		bottom: 0,
+		left: 0,
+		right: 0,
 		textAlign: 'center'
 	},
 	footerLink: {

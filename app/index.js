e9db6269ad2b17573db0e306d55ede507f67a364
Drew Blaisdell <drew.blaisdell@gmail.com>
2016-04-19 11:08:32 +0200

    Import CSS

--- app/index.js
+++ app/index.js
@@ -5,6 +5,9 @@ import { Router } from 'react-router';
 // Internal dependencies
 import routes from './routes';
 
+// Set up CSS
+import 'assets/styles/app.scss';
+
 export default function App( { history } ) {
 	return (
 		<Router history={ history } routes={ routes } />

b8922e2892124b5784999a83a926ed1ba30f7efd
Stéphane Thomas <email@stephanethomas.com>
2016-04-25 13:08:48 +0200

    Fix CSS hot reloading
    This makes sure hot reloading works correctly as previously it would only work when navigating away from the current page (new styles would be picked up and injected into the Javascript hot update but they would not be injected in a <style /> tag in the head of the page - until the user navigate back to the page).
    
    See https://github.com/kriasoft/isomorphic-style-loader/#hot-reload and https://webpack.github.io/docs/configuration.html#debug for more information.

--- webpack.config.js
+++ webpack.config.js
@@ -4,13 +4,19 @@ var webpack = require( 'webpack' ),
 	NODE_ENV = process.env.NODE_ENV || 'development';
 
 var config = {
+	// Switches loaders to debug mode. This is required to make CSS hot reloading works correctly (see
+	// http://bit.ly/1VTOHrK for more information).
+	debug: true,
+
 	devServer: {
 		port: 1337,
 		historyApiFallback: true
 	},
+
 	entry: [
 		path.join( __dirname, 'client' )
 	],
+
 	module: {
 		loaders: [
 			{
@@ -32,6 +38,7 @@ var config = {
 			}
 		]
 	},
+
 	node: {
 		console: false,
 		process: true,
@@ -41,12 +48,14 @@ var config = {
 		__dirname: 'mock',
 		fs: 'empty'
 	},
+
 	output: {
 		path: path.resolve( __dirname, 'build' ),
 		publicPath: '/build/',
 		filename: 'client.bundle.js',
 		devtoolModuleFilenameTemplate: 'app:///[resource-path]'
 	},
+
 	plugins: [
 		new webpack.DefinePlugin( {
 			'process.env': {
@@ -55,6 +64,7 @@ var config = {
 			}
 		} )
 	],
+
 	resolve: {
 		extensions: [ '', '.json', '.js', '.jsx' ],
 		modulesDirectories: [

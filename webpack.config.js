Automattic/delphin

commit ac57b2cf502aec9ba01899b45caf82e64be46041

Remove build folder and reorganize bundles
This moves the client bundle into the public/scripts directory, and the server bundle into the server/build directory in order to better separate concerns. The client bundle is then served as a static asset.

webpack.config.js
@@ -46,9 +46,9 @@ var config = {
 	},
 
 	output: {
-		path: path.resolve( __dirname, 'build' ),
-		publicPath: '/build/',
-		filename: 'client.bundle.js',
+		path: path.resolve( __dirname, 'public/scripts' ),
+		publicPath: '/public/scripts/',
+		filename: 'bundle.js',
 		devtoolModuleFilenameTemplate: 'app:///[resource-path]'
 	},
 

webpack.server.config.js
@@ -21,9 +21,9 @@ module.exports = {
 	entry: path.resolve( __dirname, 'server/index.js' ),
 
 	output: {
-		path: path.resolve( __dirname, 'build' ),
-		publicPath: '/build/',
-		filename: 'server.bundle.js'
+		path: path.resolve( __dirname, 'server/build' ),
+		publicPath: '/server/build/',
+		filename: 'bundle.js'
 	},
 
 	target: 'node',

.eslintignore
@@ -1,0 +1,1 @@
+server/build/bundle.js

.gitignore
@@ -23,8 +23,9 @@ coverage
 # node-waf configuration
 .lock-wscript
 
-# Compiled JS directory
-build
+# Compiled scripts and build directories
+public/scripts/bundle.js
+server/build
 
 # Dependency directory
 node_modules

server/index.js
@@ -40,8 +40,6 @@ if ( config( 'env' ) === 'production' ) {
 }
 
 const init = () => {
-	app.use( '/build', express.static( path.join( __dirname, '..', 'build' ) ) );
-
 	app.use( express.static( path.join( __dirname, '..', 'public' ) ) );
 
 	app.use( api() );

server/views/index.html
@@ -2,7 +2,7 @@
 <html>
 	<head>
 		<meta name="viewport" content="width=device-width, initial-scale=1">
-		<script src="/build/client.bundle.js" async></script>
+		<script src="/scripts/bundle.js" async></script>
 		<link rel="stylesheet" href="/styles/reset.css">
 	</head>
 

server/views/index.pug
@@ -4,7 +4,7 @@ html
 		if localeData
 			script(type='text/javascript')!='var localeData = ' + JSON.stringify( localeData ) + ';'
 		meta(name="viewport" content="width=device-width, initial-scale=1")
-		script(src="/build/client.bundle.js" async)
+		script(src="/scripts/bundle.js" async)
 		link(rel="stylesheet", href="/styles/reset.css")
 		style(type='text/css').
 			#{css}

commit 32a0dc125d00f9d0656dabee3c03c2b6dc38c4a9

Fix hot reloading
This was broken in ac57b2cf502aec9ba01899b45caf82e64be46041. This also removes the public path for the server since it's not necessary.

webpack.config.js
@@ -47,7 +47,7 @@ var config = {
 
 	output: {
 		path: path.resolve( __dirname, 'public/scripts' ),
-		publicPath: '/public/scripts/',
+		publicPath: '/scripts/',
 		filename: 'bundle.js',
 		devtoolModuleFilenameTemplate: 'app:///[resource-path]'
 	},

webpack.server.config.js
@@ -22,7 +22,6 @@ module.exports = {
 
 	output: {
 		path: path.resolve( __dirname, 'server/build' ),
-		publicPath: '/server/build/',
 		filename: 'bundle.js'
 	},
 
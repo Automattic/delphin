a60126d4c4c986d613e56d765dfca88519b6bcd3
Ben Dwyer <ben@scruffian.com>
2016-04-28 14:01:32 +0100

    Make the hide text translateable

--- app/components/ui/notices/notice.js
+++ app/components/ui/notices/notice.js
@@ -3,6 +3,7 @@ import React, { PropTypes } from 'react';
 import withStyles from 'isomorphic-style-loader/lib/withStyles';
 
 // Internal dependencies
+import i18n from 'lib/i18n';
 import styles from './styles.scss';
 
 const Notice = React.createClass( {
@@ -20,7 +21,9 @@ const Notice = React.createClass( {
 		return (
 			<div className={ styles.notice }>
 				{ this.props.notice.message }
-				<span className={ styles.clear } onClick={ this.clear }>Hide</span>
+				<span className={ styles.clear } onClick={ this.clear }>
+					{ i18n.translate( 'Hide' ) }
+				</span>
 			</div>
 		);
 	}

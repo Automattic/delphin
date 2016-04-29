79ffec81e09e3cc7f81ee42baf1fe69d83fcbe4c
Ben Dwyer <ben@scruffian.com>
2016-04-29 12:19:53 +0100

    Add unit tests for noticeS

---
+++ app/reducers/notices/tests/index.js
@@ -0,0 +1,47 @@
+// Internal dependencies
+import {
+	NOTICE_ADD,
+	NOTICE_REMOVE
+} from 'reducers/action-types';
+import { notices } from '..';
+
+jest.unmock( '..' );
+jest.unmock( 'lodash/uniqueId' );
+
+describe( 'notices reducer for notice add action', () => {
+	it( 'should add a notice', () => {
+		const originalState = Object.freeze( [] ),
+			notice = {
+				message: 'A error message',
+				status: 'error'
+			},
+			newState = notices( originalState, { notice, type: NOTICE_ADD } );
+
+		expect( newState ).toEqual( [
+			{
+				id: '1',
+				message: 'A error message',
+				status: 'error'
+			}
+		] );
+	} );
+} );
+
+describe( 'notices reducer for notice remove action', () => {
+	it( 'should add a notice', () => {
+		const originalState = Object.freeze( [ {
+				id: '1',
+				message: 'An error message',
+				status: 'error'
+			} ] ),
+			notice = {
+				id: '1',
+				message: 'Another error message',
+				status: 'error'
+			},
+			newState = notices( originalState, { notice, type: NOTICE_REMOVE } );
+
+		expect( newState ).toEqual( [] );
+	} );
+} );
+

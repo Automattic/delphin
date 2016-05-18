1ff308063042863a4ce5977cf162dc3e0e44ea44
Tugdual de Kerviler <dekervit@gmail.com>
2016-05-18 12:15:27 +0200

    Fix lib/domains
    Fix a bug in secondLevelDomainOf and use an external lib to parse domain so we can handle more complex TLDs such as .co.uk

--- app/lib/domains/tests/index.js
+++ app/lib/domains/tests/index.js
@@ -1,29 +1,32 @@
 jest.disableAutomock();
 
 // Internal dependencies
-import { isDomain, isValidSecondLevelDomain } from '..';
+import { isDomain, isValidSecondLevelDomain, secondLevelDomainOf } from '..';
 
 describe( 'lib/domains', () => {
-	describe( 'isDomainName', () => {
+	describe( 'isDomain', () => {
 		it( 'should match valid domain', () => {
-			expect( isDomain( 'hello.world' ) ).toBe( true );
+			expect( isDomain( 'hello.com' ) ).toBe( true );
 			expect( isDomain( 'hello-world.com' ) ).toBe( true );
 			expect( isDomain( 'hell-o.com' ) ).toBe( true );
 		} );
 
 		it( 'should not match unvalid domain', () => {
-			expect( isDomain( 'helloworld' ) ).toBe( false );
-			expect( isDomain( 'hello_world.com' ) ).toBe( false );
-			expect( isDomain( '/hello-world.com' ) ).toBe( false );
-			expect( isDomain( '.com' ) ).toBe( false );
-			expect( isDomain( 'hello.world.com' ) ).toBe( false );
-			expect( isDomain( '.world.com' ) ).toBe( false );
-			expect( isDomain( '-hello.com' ) ).toBe( false );
-			expect( isDomain( 'hello-.com' ) ).toBe( false );
-			expect( isDomain( 'hello.w' ) ).toBe( false ); // 1-letter tlds not allowed
-			expect( isDomain( 'h.ello' ) ).toBe( false ); // 1-letter slds not allowed
+			expect( isDomain() ).toBe( false ); // needs a parameter
+			expect( isDomain( null ) ).toBe( false ); // needs a string parameter
+			expect( isDomain( 'helloworld' ) ).toBe( false ); // needs a tld
+			expect( isDomain( '.com' ) ).toBe( false ); // needs an sld
+			expect( isDomain( 'hello_world.com' ) ).toBe( false ); // underscores not allowed
+			expect( isDomain( '/hello-world.com' ) ).toBe( false ); // slashes not allowed
+			expect( isDomain( 'hello.world.com' ) ).toBe( false ); // has a subdomain
+			expect( isDomain( '.world.com' ) ).toBe( false ); // can't start with a dot
+			expect( isDomain( '-hello.com' ) ).toBe( false ); // can't start with a dash
+			expect( isDomain( 'hello-.com' ) ).toBe( false ); // can't end with a dash
+			expect( isDomain( 'hello.w' ) ).toBe( false ); // invalid tld not allowed
+			expect( isDomain( 'h.ello' ) ).toBe( false ); // 1-letter sld not allowed
 		} );
 	} );
+
 	describe( 'isSecondLevelDomain', () => {
 		it( 'should match valid second-level domains', () => {
 			expect( isValidSecondLevelDomain( 'hello' ) ).toBe( true );
@@ -41,6 +44,17 @@ describe( 'lib/domains', () => {
 			expect( isValidSecondLevelDomain( '-hello' ) ).toBe( false );
 			expect( isValidSecondLevelDomain( 'hello-' ) ).toBe( false );
 			expect( isValidSecondLevelDomain( 'h' ) ).toBe( false );
+		} );
+	} );
+
+	describe( 'secondLevelDomainOf', () => {
+		it( 'should extract the SLD from domain names', () => {
+			expect( secondLevelDomainOf( 'hello.com' ) ).toBe( 'hello' );
+			expect( secondLevelDomainOf( 'hello-world.com' ) ).toBe( 'hello-world' );
+			expect( secondLevelDomainOf( 'hell-o.com' ) ).toBe( 'hell-o' );
+			expect( secondLevelDomainOf( 'helloworld' ) ).toBe( '' );
+			expect( secondLevelDomainOf( 'hello_world.com' ) ).toBe( 'hello_world' );
+			expect( secondLevelDomainOf( '.com' ) ).toBe( '' );
 		} );
 	} );
 } );

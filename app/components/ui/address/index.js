e4c274b0d9e125102f0f1d8534fad386a91073e1
Drew Blaisdell <drew.blaisdell@gmail.com>
2016-05-20 14:56:02 -0700

    Add styles to `Address`

--- app/components/ui/address/index.js
+++ app/components/ui/address/index.js
@@ -1,6 +1,10 @@
 // External dependencies
 import i18n from 'i18n-calypso';
 import React from 'react';
+import withStyles from 'isomorphic-style-loader/lib/withStyles';
+
+// Internal dependencies
+import styles from './styles.scss';
 
 class Address extends React.Component {
 	constructor( props ) {
@@ -25,45 +29,67 @@ class Address extends React.Component {
 		const { fields } = this.props;
 
 		return (
-			<div>
-				{ i18n.translate( 'Address' ) }
-				<fieldset>
-					<label>{ i18n.translate( 'Name' ) }</label>
-					<input { ...fields.name } />
-				</fieldset>
+			<div className={ styles.address }>
+				<h2 className={ styles.header }>{ i18n.translate( 'Registration Profile' ) }</h2>
+				<h3 className={ styles.subHeader }>{ i18n.translate( 'We need your contact information to claim your new domain.' ) }</h3>
+				<form className={ styles.form }>
+					<fieldset className={ styles.fieldset }>
+						<label className={ styles.label }>{ i18n.translate( 'Name' ) }</label>
+						<input
+							{ ...fields.name }
+							className={ styles.name }
+							placeholder={ i18n.translate( 'Name' ) }
+						/>
+					</fieldset>
 
-				<fieldset>
-					<label>{ i18n.translate( 'Address Line 1' ) }</label>
-					<input { ...fields.addressLine1 } />
-				</fieldset>
+					<fieldset className={ styles.fieldset }>
+						<label className={ styles.label }>{ i18n.translate( 'Address' ) }</label>
+						<input
+							{ ...fields.addressLine1 }
+							className={ styles.addressLineOne }
+							placeholder={ i18n.translate( 'Address Line 1' ) }
+						/>
+						<input
+							{ ...fields.addressLine2 }
+							className={ styles.addressLineTwo }
+							placeholder={ i18n.translate( 'Address Line 2' ) }
+						/>
+						<div className={ styles.row }>
+							<input
+								{ ...fields.city }
+								className={ styles.city }
+								placeholder={ i18n.translate( 'City' ) }
+							/>
+							<input
+								{ ...fields.state }
+								className={ styles.state }
+								placeholder={ i18n.translate( 'State' ) }
+							/>
+							<input
+								{ ...fields.zip }
+								className={ styles.zip }
+								placeholder={ i18n.translate( 'Zip' ) }
+							/>
+						</div>
+						<input
+							{ ...fields.country }
+							className={ styles.country }
+							placeholder={ i18n.translate( 'Country' ) }
+						/>
+					</fieldset>
 
-				<fieldset>
-					<label>{ i18n.translate( 'Address Line 2' ) }</label>
-					<input { ...fields.addressLine2 } />
-				</fieldset>
-
-				<fieldset>
-					<label>{ i18n.translate( 'City' ) }</label>
-					<input { ...fields.city } />
-				</fieldset>
-
-				<fieldset>
-					<label>{ i18n.translate( 'State' ) }</label>
-					<input { ...fields.state } />
-				</fieldset>
-
-				<fieldset>
-					<label>{ i18n.translate( 'Country' ) }</label>
-					<input { ...fields.country } />
-				</fieldset>
-
-				<fieldset>
-					<label>{ i18n.translate( 'Phone' ) }</label>
-					<input { ...fields.phone } />
-				</fieldset>
+					<fieldset className={ styles.fieldset }>
+						<label className={ styles.label }>{ i18n.translate( 'Phone' ) }</label>
+						<input
+							{ ...fields.phone }
+							className={ styles.phone }
+							placeholder={ i18n.translate( 'Phone' ) }k
+						/>
+					</fieldset>
+				</form>
 			</div>
 		);
 	}
 }
 
-export default Address;
+export default withStyles( styles )( Address );

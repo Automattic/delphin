c91b005bfd4c87cd16dc2372d5379d2d7ab2a762
Drew Blaisdell <drew.blaisdell@gmail.com>
2016-05-20 16:42:25 -0700

    Add action for fetching `state.countries`

---
+++ app/actions/countries.js
@@ -0,0 +1,22 @@
+// Internal dependencies
+import { addNotice } from 'actions/notices';
+import {
+	COUNTRIES_FETCH,
+	COUNTRIES_FETCH_COMPLETE,
+	WPCOM_REQUEST
+} from 'reducers/action-types';
+
+export const fetchCountries = () => ( {
+	type: WPCOM_REQUEST,
+	params: { path: '/me/transactions/supported-countries' },
+	loading: COUNTRIES_FETCH,
+	success: data => ( { type: COUNTRIES_FETCH_COMPLETE, data } ),
+	fail: error => (
+		dispatch => {
+			dispatch( addNotice( {
+				message: error.message,
+				status: 'error'
+			} ) );
+		}
+	)
+} );

5a052ac6844d3eb8decc7da47bed8aacfdc85329
Drew Blaisdell <drew.blaisdell@gmail.com>
2016-05-20 17:17:46 -0700

    Render `state.countries` on `ContactInformation`

--- app/reducers/index.js
+++ app/reducers/index.js
@@ -3,9 +3,10 @@ import { reducer as form } from 'redux-form';
 
 // Internal dependencies
 import { checkout } from './checkout';
+import { countries } from './countries';
 import { domainSearch } from './domain-search';
 import { notices } from './notices';
 import ui from './ui';
 import { user } from './user';
 
-export default { checkout, form, domainSearch, notices, ui, user };
+export default { checkout, countries, form, domainSearch, notices, ui, user };

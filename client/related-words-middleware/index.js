5c2f17a2a38f8f3867c4b3cfc40830eac3bc8eb6
Drew Blaisdell <drew.blaisdell@gmail.com>
2016-05-31 10:43:01 -0700

    Add `relatedWordsMiddleware`

---
+++ client/related-words-middleware/index.js
@@ -0,0 +1,48 @@
+// External dependencies
+import config from 'config';
+import difference from 'lodash/difference';
+import request from 'superagent';
+
+// Internal dependencies
+import { getKeywords, getRelatedWords } from 'reducers/ui/domain-search/selectors';
+import {
+	DOMAIN_SUGGESTIONS_FETCH,
+	RELATED_WORD_FETCH,
+	RELATED_WORD_FETCH_COMPLETE
+} from 'reducers/action-types';
+
+export const relatedWordsMiddleware = store => next => action => {
+	if ( action.type === DOMAIN_SUGGESTIONS_FETCH ) {
+		const state = store.getState(),
+			keywords = getKeywords( state ).map( keyword => keyword.value ),
+			existingRelatedWords = getRelatedWords( state ).map( relatedWord => relatedWord.word ),
+			wordsToFetch = difference( keywords, existingRelatedWords );
+
+		wordsToFetch.forEach( word => {
+			store.dispatch( {
+				type: RELATED_WORD_FETCH,
+				word
+			} );
+
+			request
+				.get( `http://api.wordnik.com/v4/word.json/${ word }/relatedWords` +
+					'?useCanonical=true' +
+					'&relationshipTypes=same-context,synonym' +
+					'&limitPerRelationshipType=3' +
+					`&api_key=${ config( 'wordnik_api_key' ) }` )
+				.end( ( error, response ) => {
+					const data = response.body.reduce( ( result, current ) => {
+						return result.concat( current.words );
+					}, [] );
+
+					store.dispatch( {
+						type: RELATED_WORD_FETCH_COMPLETE,
+						word,
+						data
+					} );
+				} );
+		} );
+	}
+
+	return next( action );
+};

2c3a16c58764f65871de699315704e43c19e520a
Yury Michurin <yury@nix.co.il>
2016-06-02 20:19:50 +0300

    Add implementation for translating suggested words for user's locale

--- client/related-words-middleware/index.js
+++ client/related-words-middleware/index.js
@@ -1,10 +1,13 @@
 // External dependencies
 import config from 'config';
 import difference from 'lodash/difference';
+import flatten from 'lodash/flatten';
 import request from 'superagent';
+import i18n from 'i18n-calypso';
 
 // Internal dependencies
 import { getKeywords, getRelatedWords } from 'reducers/ui/domain-search/selectors';
+import { getUserLocale } from 'reducers/user/selectors';
 import {
 	DOMAIN_SUGGESTIONS_FETCH,
 	RELATED_WORD_FETCH,
@@ -12,9 +15,46 @@ import {
 } from 'reducers/action-types';
 import { isDomain } from 'lib/domains';
 
+function requestRelatedWords( word ) {
+	return new Promise( ( resolve, reject ) => {
+		request
+			.get( `http://api.wordnik.com/v4/word.json/${ encodeURIComponent( word ) }/relatedWords` +
+				'?useCanonical=true' +
+				'&relationshipTypes=same-context,synonym' +
+				'&limitPerRelationshipType=3' +
+				`&api_key=${ config( 'wordnik_api_key' ) }` )
+			.end( ( error, response ) => {
+				if ( error ) {
+					return reject( new Error( error ) );
+				}
+
+				resolve( response.body.reduce( ( result, current ) => result.concat( current.words ), [] ) );
+			} );
+	} );
+}
+
+function translateWord( word, targetLangauge, sourceLangugage ) {
+	return new Promise( ( resolve, reject ) => {
+		request.get( 'https://www.googleapis.com/language/translate/v2' )
+			.query( {
+				key: config( 'google_translate_api_key' ),
+				target: targetLangauge,
+				source: sourceLangugage || 'en',
+				q: word
+			} ).end( ( error, response ) => {
+				if ( error ) {
+					return reject( new Error( error ) );
+				}
+
+				resolve( response.body.data.translations[ 0 ].translatedText );
+			} );
+	} );
+}
+
 export const relatedWordsMiddleware = store => next => action => {
 	if ( action.type === DOMAIN_SUGGESTIONS_FETCH ) {
 		const state = store.getState(),
+			locale = getUserLocale( state ) || i18n.getLocaleSlug(),
 			keywords = getKeywords( state ).map( keyword => keyword.value ),
 			existingRelatedWords = getRelatedWords( state ).map( relatedWord => relatedWord.word ),
 			wordsToFetch = difference( keywords, existingRelatedWords );
@@ -25,21 +65,31 @@ export const relatedWordsMiddleware = store => next => action => {
 				word
 			} );
 
-			request
-				.get( `https://api.wordnik.com/v4/word.json/${ word }/relatedWords` +
-					'?useCanonical=true' +
-					'&relationshipTypes=same-context,synonym' +
-					'&limitPerRelationshipType=3' +
-					`&api_key=${ config( 'wordnik_api_key' ) }` )
-				.end( ( error, response ) => {
-					const data = response.body.reduce( ( result, current ) => result.concat( current.words ), [] );
+			new Promise( ( resolve ) => {
+				if ( locale === 'en' ) {
+					return resolve( word );
+				}
 
-					store.dispatch( {
-						type: RELATED_WORD_FETCH_COMPLETE,
-						word,
-						data
-					} );
-				} );
+				return resolve( translateWord( word, 'en', locale ) );
+			} )
+			.then( requestRelatedWords )
+			.then( words => {
+				if ( locale === 'en' ) {
+					return words;
+				}
+
+				return Promise.all( words.map( englishWord => translateWord( englishWord, locale, 'en' ) ) )
+					.then( ( ...translatedWords ) => flatten( translatedWords ) );
+			} )
+			.then( words =>	store.dispatch( {
+				type: RELATED_WORD_FETCH_COMPLETE,
+				word,
+				data: words
+			} ) ).catch( () => store.dispatch( {
+				type: RELATED_WORD_FETCH_COMPLETE,
+				word,
+				data: null
+			} ) );
 		} );
 	}
 

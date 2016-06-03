7bf26041ccdae014bd84498a87fa77e72cdcba39
Drew Blaisdell <drew.blaisdell@gmail.com>
2016-06-03 16:51:13 -0700

    Initialize `ContactInformation` with fetched contact data

--- app/components/ui/contact-information/index.js
+++ app/components/ui/contact-information/index.js
@@ -14,9 +14,18 @@ class ContactInformation extends React.Component {
 	}
 
 	componentWillMount() {
+		if ( ! this.props.contactInformation.isRequesting && ! this.props.contactInformation.hasLoadedFromServer ) {
+			this.props.fetchContactInformation();
+		}
+
+		if ( this.props.contactInformation.hasLoadedFromServer ) {
+			this.initializeContactInformation();
+		}
+
 		if ( ! this.props.countries.isRequesting && ! this.props.countries.hasLoadedFromServer ) {
 			this.props.fetchCountries();
 		}
+
 		this.redirectIfLoggedOut();
 
 		if ( this.props.user.isLoggedIn ) {
@@ -25,11 +34,34 @@ class ContactInformation extends React.Component {
 	}
 
 	componentWillReceiveProps( nextProps ) {
+		if ( ! this.props.contactInformation.hasLoadedFromServer && nextProps.contactInformation.hasLoadedFromServer ) {
+			this.initializeContactInformation( nextProps );
+		}
+
 		this.redirectIfLoggedOut( nextProps );
 
 		if ( ! this.props.user.isLoggedIn && nextProps.user.isLoggedIn ) {
 			this.changeNameToMatchUserData( nextProps );
 		}
+	}
+
+	initializeContactInformation( props = this.props ) {
+		const form = Object.keys( props.fields ).reduce( ( result, field ) => {
+			if ( field === 'name' ) {
+				// combine the first and last name into a single `name` field
+				return Object.assign( result, {
+					name: props.contactInformation.data.firstName + ' ' + props.contactInformation.data.lastName
+				} );
+			}
+
+			return Object.assign( result, { [ field ]: props.contactInformation.data[ field ] || '' } );
+		}, {} );
+
+		props.initializeForm( form );
+	}
+
+	isDataLoading() {
+		return ! this.props.contactInformation.hasLoadedFromServer || ! this.props.countries.hasLoadedFromServer;
 	}
 
 	redirectIfLoggedOut( props = this.props ) {
@@ -79,6 +111,7 @@ class ContactInformation extends React.Component {
 					<fieldset className={ styles.fieldset }>
 						<label className={ styles.label }>{ i18n.translate( 'Name' ) }</label>
 						<input
+							disabled={ this.isDataLoading() }
 							{ ...fields.name }
 							autoFocus
 							className={ styles.name }
@@ -89,6 +122,7 @@ class ContactInformation extends React.Component {
 					<fieldset className={ styles.fieldset }>
 						<label className={ styles.label }>{ i18n.translate( 'Organization' ) }</label>
 						<input
+							disabled={ this.isDataLoading() }
 							{ ...fields.organization }
 							className={ styles.organization }
 							placeholder={ i18n.translate( 'Organization' ) }
@@ -98,36 +132,41 @@ class ContactInformation extends React.Component {
 					<fieldset className={ styles.fieldset }>
 						<label className={ styles.label }>{ i18n.translate( 'Address' ) }</label>
 						<input
-							{ ...fields.addressLine1 }
-							className={ styles.addressLineOne }
+							disabled={ this.isDataLoading() }
+							{ ...fields.address1 }
+							className={ styles.addressOne }
 							placeholder={ i18n.translate( 'Address Line 1' ) }
 						/>
 						<input
-							{ ...fields.addressLine2 }
-							className={ styles.addressLineTwo }
+							disabled={ this.isDataLoading() }
+							{ ...fields.address2 }
+							className={ styles.addressTwo }
 							placeholder={ i18n.translate( 'Address Line 2' ) }
 						/>
 						<div className={ styles.row }>
 							<input
+								disabled={ this.isDataLoading() }
 								{ ...fields.city }
 								className={ styles.city }
 								placeholder={ i18n.translate( 'City' ) }
 							/>
 							<input
+								disabled={ this.isDataLoading() }
 								{ ...fields.state }
 								className={ styles.state }
 								placeholder={ i18n.translate( 'State' ) }
 							/>
 							<input
-								{ ...fields.zip }
-								className={ styles.zip }
+								disabled={ this.isDataLoading() }
+								{ ...fields.postalCode }
+								className={ styles.postalCode }
 								placeholder={ i18n.translate( 'Zip' ) }
 							/>
 						</div>
 						<select
-							{ ...fields.country }
-							disabled={ ! countries.hasLoadedFromServer }
-							className={ styles.country }>
+							{ ...fields.countryCode }
+							disabled={ this.isDataLoading() }
+							className={ styles.countryCode }>
 							<option>{ i18n.translate( 'Select Country' ) }</option>
 							<option value=" " key="separator" disabled />
 							{ countries.hasLoadedFromServer && countries.data.map( ( country, index ) => (
@@ -141,6 +180,7 @@ class ContactInformation extends React.Component {
 					<fieldset className={ styles.fieldset }>
 						<label className={ styles.label }>{ i18n.translate( 'Fax' ) }</label>
 						<input
+							disabled={ this.isDataLoading() }
 							{ ...fields.fax }
 							className={ styles.fax }
 							placeholder={ i18n.translate( 'Fax' ) }
@@ -150,6 +190,7 @@ class ContactInformation extends React.Component {
 					<fieldset className={ styles.fieldset }>
 						<label className={ styles.label }>{ i18n.translate( 'Phone' ) }</label>
 						<input
+							disabled={ this.isDataLoading() }
 							{ ...fields.phone }
 							className={ styles.phone }
 							placeholder={ i18n.translate( 'Phone' ) }

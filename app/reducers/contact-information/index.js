fdeaaa635473e794a502c2d27814c398830d1f83
Drew Blaisdell <drew.blaisdell@gmail.com>
2016-06-03 16:03:03 -0700

    Add `state.contactInformation` reducer

---
+++ app/reducers/contact-information/index.js
@@ -0,0 +1,31 @@
+// Internal dependencies
+import { camelize } from 'lib/formatters';
+import {
+	CONTACT_INFORMATION_FETCH,
+	CONTACT_INFORMATION_FETCH_COMPLETE
+} from 'reducers/action-types';
+
+export const initialState = {
+	isRequesting: false,
+	hasLoadedFromServer: false,
+	data: null
+};
+
+export const contactInformation = ( state = initialState, action ) => {
+	const { data, type } = action;
+
+	switch ( type ) {
+		case CONTACT_INFORMATION_FETCH:
+			return Object.assign( {}, state, { isRequesting: true } );
+
+		case CONTACT_INFORMATION_FETCH_COMPLETE:
+			return Object.assign( {}, state, {
+				isRequesting: false,
+				hasLoadedFromServer: true,
+				data: camelize( data )
+			} );
+
+		default:
+			return state;
+	}
+};

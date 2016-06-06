86b3b70d43feca2426c144c72c05cb579d8ac163
Drew Blaisdell <drew.blaisdell@gmail.com>
2016-06-06 16:56:48 -0700

    Split `state.user` into multiple reducers
    This will allow us to nest reducers that contain user data (e.g. `contactInformation`)
    under `state.user`.

---
+++ app/reducers/user/settings.js
@@ -0,0 +1,52 @@
+// External dependencies
+import merge from 'lodash/merge';
+
+// Internal dependencies
+import {
+	FETCH_USER,
+	FETCH_USER_COMPLETE,
+	FETCH_USER_FAIL,
+	LOGOUT_USER
+} from 'reducers/action-types';
+
+export const initialState = {
+	isRequesting: false,
+	hasLoadedFromServer: false,
+	data: null
+};
+
+export const settings = ( state = initialState, action ) => {
+	const {
+		email,
+		firstName,
+		lastName,
+		locale,
+		type
+	} = action;
+
+	switch ( type ) {
+		case FETCH_USER:
+			return Object.assign( {}, state, {
+				isRequesting: true
+			} );
+
+		case FETCH_USER_COMPLETE:
+			return merge( {}, state, {
+				hasLoadedFromServer: true,
+				isRequesting: false,
+				data: {
+					firstName,
+					lastName,
+					email,
+					locale
+				}
+			} );
+
+		case FETCH_USER_FAIL:
+		case LOGOUT_USER:
+			return initialState;
+
+		default:
+			return state;
+	}
+};
--- app/reducers/user/tests/index.js
+++ app/reducers/user/tests/index.js
@@ -2,111 +2,23 @@ jest.disableAutomock();
 
 // Internal dependencies
 import {
-	CONNECT_USER,
-	CONNECT_USER_CLEAR,
-	CONNECT_USER_COMPLETE,
-	FETCH_USER,
 	FETCH_USER_COMPLETE,
-	VERIFY_USER,
-	VERIFY_USER_COMPLETE
+	LOGOUT_USER
 } from 'reducers/action-types';
-import { user, initialState } from '..';
+import { isLoggedIn } from '..';
 
 describe( 'state.user', () => {
-	it( 'should return the initial state', () => {
-		expect( user( undefined, { type: 'RANDOM' } ) ).toEqual( initialState );
-	} );
-
-	it( 'should update `email` and `isRequesting` when the user is created', () => {
-		expect( user( undefined, {
-			type: CONNECT_USER,
-			email: 'foo@bar.com'
-		} ) ).toEqual( {
-			intention: null,
-			isLoggedIn: false,
-			isRequesting: true,
-			wasCreated: false,
-			data: {
-				bearerToken: null,
-				firstName: null,
-				lastName: null,
-				email: 'foo@bar.com',
-				twoFactorAuthenticationEnabled: null
-			}
+	describe( 'isLoggedIn', () => {
+		it( 'should return a false `isLoggedIn` at first', () => {
+			expect( isLoggedIn( undefined, { type: 'UNRELATED' } ) ).toBe( false );
 		} );
-	} );
 
-	it( 'should update `wasCreated` when the user creation completes', () => {
-		expect( user( undefined, {
-			type: CONNECT_USER_COMPLETE,
-			email: 'foo@bar.com',
-			twoFactorAuthenticationEnabled: false
-		} ) ).toEqual( {
-			intention: null,
-			isLoggedIn: false,
-			isRequesting: false,
-			wasCreated: true,
-			data: {
-				bearerToken: null,
-				firstName: null,
-				lastName: null,
-				email: 'foo@bar.com',
-				twoFactorAuthenticationEnabled: false
-			}
+		it( 'should return a true `isLoggedIn` after fetching completes', () => {
+			expect( isLoggedIn( false, { type: FETCH_USER_COMPLETE } ) ).toBe( true );
 		} );
-	} );
 
-	it( 'should update `isRequesting` when verifying', () => {
-		expect( user( undefined, { type: VERIFY_USER } ).isRequesting ).toBe( true );
-	} );
-
-	it( 'should update `isRequesting` and `bearerToken`, but not `isLoggedIn`, when the user is verified', () => {
-		const result = user( undefined, { type: VERIFY_USER_COMPLETE, bearerToken: 'foobar' } );
-
-		expect( result.isRequesting ).toBe( false );
-		expect( result.isLoggedIn ).toBe( false );
-		expect( result.data.bearerToken ).toBe( 'foobar' );
-	} );
-
-	it( 'should reset `wasCreated` when a `CONNECT_USER_CLEAR` action is triggered', () => {
-		expect( user( {
-			intention: null,
-			isLoggedIn: false,
-			isRequesting: false,
-			wasCreated: true,
-			data: { bearerToken: null, email: 'foo@bar.com', twoFactorAuthenticationEnabled: false }
-		}, { type: CONNECT_USER_CLEAR } ) ).toEqual( {
-			intention: null,
-			isLoggedIn: false,
-			isRequesting: false,
-			wasCreated: false,
-			data: { bearerToken: null, email: 'foo@bar.com', twoFactorAuthenticationEnabled: false }
-		} );
-	} );
-
-	it( 'should update `isRequesting` when the user is fetched', () => {
-		expect( user( undefined, { type: FETCH_USER } ).isRequesting ).toBe( true );
-	} );
-
-	it( 'should update multiple user properties when when the user fetching is completed', () => {
-		expect( user( Object.assign( {}, initialState, { isRequesting: true } ), {
-			type: FETCH_USER_COMPLETE,
-			bearerToken: 'foo',
-			firstName: 'Foo',
-			lastName: 'Bar',
-			email: 'foo@bar.com'
-		} ) ).toEqual( {
-			intention: null,
-			isLoggedIn: true,
-			isRequesting: false,
-			wasCreated: false,
-			data: {
-				bearerToken: 'foo',
-				firstName: 'Foo',
-				lastName: 'Bar',
-				email: 'foo@bar.com',
-				twoFactorAuthenticationEnabled: null
-			}
+		it( 'should return a false `isLoggedIn` after the user is logged out', () => {
+			expect( isLoggedIn( true, { type: LOGOUT_USER } ) ).toBe( false );
 		} );
 	} );
 } );

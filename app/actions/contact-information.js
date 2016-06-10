1ac7e85ea0b35814657c3d67e64da094571b433a
Stéphane Thomas <email@stephanethomas.com>
2016-06-10 12:26:56 +0200

    Contact Information: add form validation (#151)
    * Validate contact information via the API
    
    * Update Redux Form package to latest version
    
    * Rename identifier of postal code field in Contact Information page
    
    This makes sure the field's identifier matches the one of the data returned by the API.
    
    * Use `redux-form` actions in `validateContactInformation`
    
    This lets us associate these with the form in `state.form.contact-information`
    
    * Normalize errors when validating contact information
    
    * Normalize payload for validating contact information
    
    * Add tests for normalization functions
    
    * Rename functions that camelize or snakeify object keys to be more explicit
    
    * Retrieves list of countries supported for domain registrations
    
    We were pulling the wrong list of countries.
    
    * Split the name field on the Contact Information page
    
    This adds two fields instead: one for the first name and another one for the last name. That's what the WordPress.com API as well as other third party APIs expect, and it will make our code way simpler.

--- app/actions/contact-information.js
+++ app/actions/contact-information.js
@@ -1,9 +1,15 @@
+// External dependencies
+import camelCase from 'lodash/camelCase';
+import { startAsyncValidation, stopAsyncValidation } from 'redux-form';
+
 // Internal dependencies
+import { addNotice } from 'actions/notices';
 import {
 	CONTACT_INFORMATION_FETCH,
 	CONTACT_INFORMATION_FETCH_COMPLETE,
 	WPCOM_REQUEST
 } from 'reducers/action-types';
+import { snakeifyKeys } from 'lib/formatters';
 
 export const fetchContactInformation = () => ( {
 	type: WPCOM_REQUEST,
@@ -15,3 +21,52 @@ export const fetchContactInformation = () => ( {
 		data
 	} )
 } );
+
+/**
+ * Validates the specified contact information.
+ *
+ * @param {array} domainNames - list of domain names to validate the contact information against
+ * @param {object} contactInformation - user's contact information
+ * @returns {object} - the corresponding action object
+ */
+export function validateContactInformation( domainNames, contactInformation ) {
+	return {
+		type: WPCOM_REQUEST,
+		method: 'post',
+		params: { path: '/me/domain-contact-information/validate' },
+		payload: snakeifyKeys( { domainNames, contactInformation } ),
+		loading: () => {
+			return dispatch => {
+				dispatch( startAsyncValidation( 'contact-information' ) );
+			};
+		},
+		success: data => {
+			return dispatch => {
+				const { success, messages } = data;
+				let errors;
+
+				if ( ! success ) {
+					errors = Object.keys( messages ).reduce( ( result, fieldName ) => {
+						// redux-form only allows objects or strings as error values, so we nest the array of errors
+						// under an arbitrary `data` property.
+						result[ camelCase( fieldName ) ] = { data: messages[ fieldName ] };
+
+						return result;
+					}, {} );
+				}
+
+				dispatch( stopAsyncValidation( 'contact-information', errors ) );
+			};
+		},
+		fail: ( error ) => {
+			return dispatch => {
+				dispatch( addNotice( {
+					message: error.message,
+					status: 'error'
+				} ) );
+
+				return Promise.reject( error );
+			};
+		}
+	};
+}

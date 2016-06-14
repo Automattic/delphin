e8910266c7192daaa1bf7f8a3609a2eac80feed6
Stéphane Thomas <email@stephanethomas.com>
2016-06-14 18:35:24 +0200

    Add action to fetch a list of states for a given country

--- app/actions/territories.js
+++ app/actions/territories.js
@@ -4,8 +4,12 @@ import {
 	COUNTRIES_FETCH,
 	COUNTRIES_FETCH_COMPLETE,
 	COUNTRIES_FETCH_ERROR,
+	STATES_FETCH,
+	STATES_FETCH_COMPLETE,
+	STATES_FETCH_ERROR,
 	WPCOM_REQUEST
 } from 'reducers/action-types';
+import { getStates } from 'reducers/territories/selectors';
 
 export const fetchCountries = () => ( {
 	type: WPCOM_REQUEST,
@@ -23,3 +27,31 @@ export const fetchCountries = () => ( {
 		}
 	)
 } );
+
+export const fetchStates = ( countryCode ) => {
+	return ( dispatch, getState ) => {
+		const states = getStates( getState(), countryCode );
+
+		// Fetches the list of states for a given country only once
+		if ( states.hasLoadedFromServer ) {
+			return;
+		}
+
+		dispatch( {
+			type: WPCOM_REQUEST,
+			params: { path: `/domains/supported-states/${ countryCode }` },
+			loading: { type: STATES_FETCH, countryCode },
+			success: data => ( { type: STATES_FETCH_COMPLETE, countryCode, data } ),
+			fail: error => (
+				innerDispatch => {
+					innerDispatch( addNotice( {
+						message: error.message,
+						status: 'error'
+					} ) );
+
+					innerDispatch( { type: STATES_FETCH_ERROR, countryCode } );
+				}
+			)
+		} );
+	};
+};

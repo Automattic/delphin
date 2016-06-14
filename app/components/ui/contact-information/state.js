967951e94b0cf5345eb7bd2d903c3fd47e082713
Stéphane Thomas <email@stephanethomas.com>
2016-06-14 18:35:43 +0200

    Display a list of states for specific countries on the Contact Information page

---
+++ app/components/ui/contact-information/state.js
@@ -0,0 +1,41 @@
+// External dependencies
+import i18n from 'i18n-calypso';
+import isEmpty from 'lodash/isEmpty';
+import React, { PropTypes } from 'react';
+import withStyles from 'isomorphic-style-loader/lib/withStyles';
+
+// Internal dependencies
+import styles from './styles.scss';
+
+const State = ( { disabled, field, states } ) => {
+	if ( isEmpty( states.data ) ) {
+		return (
+			<input
+				{ ...field }
+				className={ styles.state }
+				disabled={ disabled }
+				placeholder={ i18n.translate( 'State' ) } />
+		);
+	}
+
+	return (
+		<select
+			{ ...field }
+			className={ styles.state }
+			disabled={ disabled }>
+			<option value="" disabled>{ i18n.translate( 'State' ) }</option>
+			<option disabled />
+			{ states.data.map( ( state ) => (
+				<option value={ state.code } key={ state.code }>{ state.name }</option>
+			) ) }
+		</select>
+	);
+};
+
+State.propTypes = {
+	disabled: PropTypes.bool.isRequired,
+	field: PropTypes.object.isRequired,
+	states: PropTypes.object.isRequired
+};
+
+export default withStyles( styles )( State );

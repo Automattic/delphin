8e42d0131330ada32439d1266e0a0bb0bd6c30a7
Tugdual de Kerviler <dekervit@gmail.com>
2016-06-15 17:34:34 +0200

    Restore a test wrongfully removed in #183 (fixed)

--- app/reducers/checkout/tests/index.js
+++ app/reducers/checkout/tests/index.js
@@ -24,7 +24,7 @@ describe( 'checkout reducer', () => {
 	} );
 
 	it( 'should return original state when action is empty', () => {
-		const originalState = Object.freeze( { domain: 'example.com' } ),
+		const originalState = Object.freeze( { domain: 'example.com', cost: '$5.00' } ),
 			newState = checkout( originalState, {} );
 
 		expect( newState ).toEqual( originalState );
@@ -40,31 +40,33 @@ describe( 'checkout reducer', () => {
 
 describe( 'checkout reducer for select domain action', () => {
 	it( 'should return state with undefined domain when provided domain is undefined', () => {
-		const originalState = Object.freeze( { domain: 'example.com' } ),
+		const originalState = Object.freeze( { domain: 'example.com', cost: '$10.00' } ),
 			newState = checkout( originalState, { type: DOMAIN_SELECT } );
 
 		expect( newState ).toEqual( { domain: undefined } );
 	} );
 
 	it( 'should return state with new domain', () => {
-		const originalState = Object.freeze( { domain: 'example.com' } ),
-			newState = checkout( originalState, { domain: 'wordpress.org', type: DOMAIN_SELECT } );
+		const originalState = Object.freeze( { domain: 'example.com', cost: '$15.00' } ),
+			newState = checkout( originalState, { value: { domain_name: 'wordpress.org', cost: '$20.00' }, type: DOMAIN_SELECT } );
 
-		expect( newState ).toEqual( { domain: 'wordpress.org' } );
+		expect( newState ).toEqual( { domain: 'wordpress.org', cost: '$20.00' } );
 	} );
 } );
 
 describe( 'checkout reducer for create site complete action', () => {
 	it( 'should return state with new site', () => {
-		const originalState = Object.freeze( { domain: 'example.com' } ),
+		const originalState = Object.freeze( { domain: 'example.com', cost: '$25.00' } ),
 			newState = checkout( originalState, {
 				blogId: 1234,
 				domain: 'example.com',
+				cost: '$25.00',
 				type: CREATE_SITE_COMPLETE
 			} );
 
 		expect( newState ).toEqual( {
 			domain: 'example.com',
+			cost: '$25.00',
 			site: {
 				blogId: 1234,
 				domain: 'example.com'
@@ -75,7 +77,7 @@ describe( 'checkout reducer for create site complete action', () => {
 
 describe( 'checkout reducer for create transaction complete action', () => {
 	it( 'should return state with new user', () => {
-		const originalState = Object.freeze( { domain: 'example.com' } ),
+		const originalState = Object.freeze( { domain: 'example.com', cost: '$30.00' } ),
 			newState = checkout( originalState, {
 				form: {
 					cvv: 'johndoe@example.com',
@@ -89,6 +91,7 @@ describe( 'checkout reducer for create transaction complete action', () => {
 
 		expect( newState ).toEqual( {
 			domain: 'example.com',
+			cost: '$30.00',
 			transaction: {
 				cvv: 'johndoe@example.com',
 				'credit-card-number': 4485839645479525,

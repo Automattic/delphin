776085023c3592f104f69f6ce8d85d8b7625a358
Drew Blaisdell <drew.blaisdell@gmail.com>
2016-06-15 12:09:40 -0400

    Remove `masking` from `credit-card-details`
    We don't need this right now

--- app/lib/credit-card-details/index.js
+++ app/lib/credit-card-details/index.js
@@ -1,12 +1,9 @@
 /**
  * Internal dependencies
  */
-var masking = require( './masking' ),
-	validation = require( './validation' );
+var validation = require( './validation' );
 
 module.exports = {
 	getCreditCardType: validation.getCreditCardType,
-	maskField: masking.maskField,
-	unmaskField: masking.unmaskField,
 	validateCardDetails: validation.validateCardDetails
 };

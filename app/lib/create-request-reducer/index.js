3b77357bf85b8a5be24f3c70855f34867227ab55
Drew Blaisdell <drew.blaisdell@gmail.com>
2016-06-16 16:13:23 -0400

    Add `create-request-reducer`

---
+++ app/lib/create-request-reducer/index.js
@@ -0,0 +1,34 @@
+// External dependencies
+import omit from 'lodash/omit';
+
+export const initialState = {
+	hasLoadedFromServer: false,
+	isRequesting: false,
+	data: null,
+	error: null
+};
+
+export const createRequestReducer = ( { loading, success, fail } = {}, additionalReducer = x => x ) => ( state = initialState, action ) => {
+	const { type, error } = action;
+
+	if ( loading && type === loading ) {
+		return Object.assign( {}, state, { isRequesting: true } );
+	}
+
+	if ( success && type === success ) {
+		return Object.assign( {}, state, {
+			hasLoadedFromServer: true,
+			isRequesting: false,
+			data: omit( action, 'type' )
+		} );
+	}
+
+	if ( fail && type === fail ) {
+		return Object.assign( {}, state, {
+			isRequesting: false,
+			error
+		} );
+	}
+
+	return additionalReducer( state, action );
+};

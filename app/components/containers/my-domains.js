129b69937743e2f019cc1d2a646e7f89e2f44338
Tugdual de Kerviler <dekervit@gmail.com>
2016-06-17 13:29:49 +0200

    Add an UI state for MyDomains to be able to show the details of each Domain Card

--- app/components/containers/my-domains.js
+++ app/components/containers/my-domains.js
@@ -1,39 +1,45 @@
 // External dependencies
-import { push } from 'react-router-redux';
+import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
 
 // Internal dependencies
 import { fetchMyDomains } from 'actions/my-domains';
 import { isLoggedIn } from 'reducers/user/selectors';
 import MyDomains from 'components/ui/my-domains';
+import { showDomainDetails, hideDomainDetails } from 'actions/ui/my-domains';
 
 export default connect(
-	( state, ownProps ) => ( {
+	state => ( {
 		isRequesting: state.domainSearch.isRequesting,
 		isLoggedIn: isLoggedIn( state ),
 		domains: state.user.myDomains
 	} ),
-	( dispatch, ownProps ) => ( {
-		fetchMyDomains() {
-			dispatch( fetchMyDomains() );
-			setTimeout( function() {
-				dispatch( {
-					type: 'MY_DOMAINS_FETCH_COMPLETE',
-					results: [
-						{
-							domain_name: 'helloworld.live'
-						},
-						{
-							domain_name: 'foobar.live'
-						},
-						{
-							domain_name: 'ilovepoetry.live'
-						}
-					]
-				} );
-			} );
-		}
-	} ),
+	dispatch => (
+		bindActionCreators( {
+			// TODO: should be replaced with just `fetchMyDomains` once we get real data from the server
+			fetchMyDomains: ( domainName ) => {
+				setTimeout( function() {
+					dispatch( {
+						type: 'MY_DOMAINS_FETCH_COMPLETE',
+						results: [
+							{
+								domain_name: 'helloworld.live'
+							},
+							{
+								domain_name: 'foobar.live'
+							},
+							{
+								domain_name: 'ilovepoetry.live'
+							}
+						]
+					} );
+				}, 2000 );
+				return fetchMyDomains( domainName );
+			},
+			showDomainDetails,
+			hideDomainDetails
+		}, dispatch )
+	),
 	( stateProps, dispatchProps ) => Object.assign( {}, stateProps, dispatchProps, {
 		fetchMyDomains() {
 			dispatchProps.fetchMyDomains();

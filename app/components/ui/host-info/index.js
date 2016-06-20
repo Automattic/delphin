0c6e41bcad7599b672dfb2c0ef6038f7b7725f5a
Drew Blaisdell <drew.blaisdell@gmail.com>
2016-06-20 18:12:52 -0400

    Update `HostInfo` to use `app/lib/hosts/`

--- app/components/ui/host-info/index.js
+++ app/components/ui/host-info/index.js
@@ -1,10 +1,12 @@
 // External dependencies
+import find from 'lodash/find';
 import i18n from 'i18n-calypso';
-import React, { PropTypes } from 'react';
+import React from 'react';
 import { Link } from 'react-router';
 import withStyles from 'isomorphic-style-loader/lib/withStyles';
 
 // Internal dependencies
+import { hosts } from 'lib/hosts';
 import styles from './styles.scss';
 
 function getCost( cost ) {
@@ -13,21 +15,23 @@ function getCost( cost ) {
 
 class HostInfo extends React.Component {
 	render() {
-		return <section>
-			<Link to={ this.props.backUrl }>←  { i18n.translate( 'View All' ) }</Link>
+		const host = find( hosts, { name: this.props.hostName } );
 
-			<h2>{ this.props.host.name }</h2>
+		return <section>
+			<Link to={ this.props.backUrl }>← { i18n.translate( 'View All' ) }</Link>
+
+			<h2>{ host.name }</h2>
 			<a>{ i18n.translate( 'Connect Now' ) }</a>
-			<p className={ styles.feature }>{ this.props.host.longDescription[0] }</p>
+			<p className={ styles.feature }>{ host.longDescription[ 0 ] }</p>
 			<dl className={ styles.credentials } >
 				<dt className={ styles.credentialTitle }>COST</dt>
-				<dd className={ styles.credentialCost }>{ this.props.host.price ? getCost( this.props.host.price ) : i18n.translate( 'Free' ) }</dd>
+				<dd className={ styles.credentialCost }>{ host.price ? getCost( host.price ) : i18n.translate( 'Free' ) }</dd>
 
 				<dt className={ styles.credentialTitle }>BEST FOR</dt>
-				{ this.props.host.bestForTags.map( ( bestTag ) => <dd key={ bestTag } className={ styles.bestForTag }>{ bestTag }</dd> ) }
+				{ host.bestForTags.map( ( bestTag ) => <dd key={ bestTag } className={ styles.bestForTag }>{ bestTag }</dd> ) }
 			</dl>
 			{
-				this.props.host.longDescription.slice( 1 ).map( ( paragraph, index ) => <p key={ this.props.host.name + 'description' + index } className={ styles.description }>{ paragraph }</p> )
+				host.longDescription.slice( 1 ).map( ( paragraph, index ) => <p key={ host.name + 'description' + index } className={ styles.description }>{ paragraph }</p> )
 			}
 		</section>;
 	}

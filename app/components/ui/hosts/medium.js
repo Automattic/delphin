b7c9aad8007b9cfb1a53417c0cf15954616bee87
Ben Dwyer <ben@scruffian.com>
2016-06-20 17:54:17 +0100

    CSS fixes

--- app/components/ui/hosts/medium.js
+++ app/components/ui/hosts/medium.js
@@ -3,7 +3,7 @@ import React from 'react';
 
 export default () => (
 	<svg width="106px" height="90px" viewBox="0 0 106 90" version="1.1" xmlns="http://www.w3.org/2000/svg">
-		<g id="Page-1" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+		<g id="Page-1" stroke="none" strokeWidth="1" fill="none" fillRule="evenodd">
 			<g id="Group">
 				<path d="M72.898,0.307609431 C71.304,-0.489390569 70,0.316609431 70,2.09860943 L70,72.9876094 L99.089,87.5326094 C102.29,89.1326094 104.885,88.0126094 104.885,85.0306094 L104.885,16.7666094 C104.885,16.4816094 104.723,16.2196094 104.468,16.0916094 L72.898,0.307609431" id="Fill-1" fill="#CCCDC9"></path>
 				<path d="M69.186,1.66544462 C70.12,0.147444615 72.188,-0.441555385 73.782,0.355444615 L105.388,16.1574446 C105.593,16.2604446 105.666,16.5164446 105.546,16.7114446 L70.884,73.0354446 L36,55.5934446 L69.186,1.66544462 Z" id="Fill-2" fill="#9EA1A0"></path>

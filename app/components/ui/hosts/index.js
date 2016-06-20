99f5efd48f1c9f0065510a238459e20c170ed4ac
Drew Blaisdell <drew.blaisdell@gmail.com>
2016-06-20 17:47:26 -0400

    Add missing `key` prop in `Hosts`

--- app/components/ui/hosts/index.js
+++ app/components/ui/hosts/index.js
@@ -14,7 +14,7 @@ class Hosts extends React.Component {
 			<h2 className={ styles.heading2 }>{ i18n.translate( 'Where would you like to create your blog?' ) }</h2>
 			<h3 className={ styles.heading3 }>{ i18n.translate( 'Here are a few powerful options that can be connected automatically to your domain using MagicDomains.' ) }</h3>
 			<ul className={ styles.hostThumbnailsList } >
-				{ hosts.map( ( host ) => <HostThumbnailContainer { ...host } /> ) }
+				{ hosts.map( host => <HostThumbnailContainer key={ host.name } { ...host } /> ) }
 			</ul>
 		</section>;
 	}

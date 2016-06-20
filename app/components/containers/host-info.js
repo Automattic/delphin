f52b224ad27a3a8a43aceee5d63ed7e6980a474a
Yury Michurin <yury@nix.co.il>
2016-06-20 16:27:46 +0300

    Modified styles and structure

--- app/components/containers/host-info.js
+++ app/components/containers/host-info.js
@@ -1,13 +1,18 @@
 // External dependencies
 import { connect } from 'react-redux';
+import config from 'config';
 
 // Internal dependencies
 import HostInfo from 'components/ui/host-info';
+import { getPath } from 'routes';
 
 export default connect(
 	( state, ownProps ) => ( {
-		hostName: ownProps.params.hostName
+		hostName: ownProps.params.hostName,
+		host: config( 'hosts' ).find( ( host ) => host.name === ownProps.params.hostName ),
+		backUrl: getPath( 'hosts' )
 	} ),
 	dispatch => ( {
+		
 	} )
 )( HostInfo );
--- app/components/ui/host-info/index.js
+++ app/components/ui/host-info/index.js
@@ -1,18 +1,34 @@
 // External dependencies
 import i18n from 'i18n-calypso';
-import randomWords from 'random-words';
 import React, { PropTypes } from 'react';
-import ReactCSSTransitionGroup from 'react-addons-css-transition-group';
-import ReactDOM from 'react-dom';
+import { Link } from 'react-router';
 import withStyles from 'isomorphic-style-loader/lib/withStyles';
 
 // Internal dependencies
 import styles from './styles.scss';
 
+function getCost( cost ) {
+	return `${cost.start} - ${cost.end} ${cost.currency}/month`;
+}
+
 class HostInfo extends React.Component {
 	render() {
 		return <section>
-			Requested host: { this.props.hostName }
+			<Link to={ this.props.backUrl }>←  { i18n.translate( 'View All' ) }</Link>
+
+			<h2>{ this.props.host.name }</h2>
+			<a>{ i18n.translate( 'Connect Now' ) }</a>
+			<p className={ styles.feature }>{ this.props.host.longDescription[0] }</p>
+			<dl className={ styles.credentials } >
+				<dt className={ styles.credentialTitle }>COST</dt>
+				<dd className={ styles.credentialCost }>{ this.props.host.price ? getCost( this.props.host.price ) : i18n.translate( 'Free' ) }</dd>
+
+				<dt className={ styles.credentialTitle }>BEST FOR</dt>
+				{ this.props.host.bestForTags.map( ( bestTag ) => <dd key={ bestTag } className={ styles.bestForTag }>{ bestTag }</dd> ) }
+			</dl>
+			{
+				this.props.host.longDescription.slice( 1 ).map( ( paragraph, index ) => <p key={ this.props.host.name + 'description' + index } className={ styles.description }>{ paragraph }</p> )
+			}
 		</section>;
 	}
 }

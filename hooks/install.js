803d2410857829bd26b54c5d3ecdcdaf215f5caf
Stéphane Thomas <email@stephanethomas.com>
2016-06-20 19:57:28 +0200

    Rename hook to describe the task performed
    This script will be used at pre-push time very soon.

--- hooks/install.js
+++ hooks/install.js
@@ -1,7 +1,7 @@
 const fs = require( 'fs' );
 const path = require( 'path' );
 
-const customHook = path.resolve( __dirname, 'pre-commit' );
+const customHook = path.resolve( __dirname, 'lint' );
 const defaultHook = path.resolve( __dirname, '..', '.git', 'hooks', 'pre-commit' );
 
 fs.access( defaultHook, ( error ) => {

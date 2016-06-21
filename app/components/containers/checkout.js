d726efadd53072e69e9eb227528a78f785cf7521
Stéphane Thomas <email@stephanethomas.com>
2016-06-21 17:00:30 +0200

    Fixed undefined error thrown upon Checkout page load
    This should only happen when the Checkout page is accessed client-side whereas the user has not yet selected a domain nor filled in any contact information.

--- app/components/containers/checkout.js
+++ app/components/containers/checkout.js
@@ -15,6 +15,10 @@ import { isLoggedIn, getUserSettings } from 'reducers/user/selectors';
  * @returns {string} - the full name
  */
 export const getFullName = state => {
+	if ( ! state.form.contactInformation ) {
+		return '';
+	}
+
 	const { firstName: { value: firstName }, lastName: { value: lastName } } = state.form.contactInformation;
 
 	return `${ firstName } ${ lastName }`;

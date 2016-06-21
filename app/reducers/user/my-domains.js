7d8aed91d5270159169661104972e7eb58c79f5c
Tugdual de Kerviler <dekervit@gmail.com>
2016-06-21 10:55:05 +0200

    Use lib/create-request-reducer for the myDomains reducer

--- app/reducers/user/my-domains.js
+++ app/reducers/user/my-domains.js
@@ -1,38 +1,13 @@
 // Internal dependencies
+import { createRequestReducer } from 'lib/create-request-reducer';
 import {
 	MY_DOMAINS_FETCH,
 	MY_DOMAINS_FETCH_COMPLETE,
 	MY_DOMAINS_FETCH_FAIL
 } from 'reducers/action-types';
 
-export const initialState = {
-	isRequesting: false,
-	hasLoadedFromServer: false,
-	data: null
-};
-
-export const myDomains = ( state = initialState, action ) => {
-	const { type, results } = action;
-
-	switch ( type ) {
-		case MY_DOMAINS_FETCH:
-			return Object.assign( {}, state, {
-				hasLoadedFromServer: false,
-				isRequesting: true,
-				data: []
-			} );
-
-		case MY_DOMAINS_FETCH_COMPLETE:
-			return Object.assign( {}, state, {
-				hasLoadedFromServer: true,
-				isRequesting: false,
-				data: results
-			} );
-
-		case MY_DOMAINS_FETCH_FAIL:
-			return initialState;
-
-		default:
-			return state;
-	}
-};
+export const myDomains = createRequestReducer( {
+	loading: MY_DOMAINS_FETCH,
+	success: MY_DOMAINS_FETCH_COMPLETE,
+	fail: MY_DOMAINS_FETCH_FAIL
+} );

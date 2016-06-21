81746da513c57f2fe81e33f38a38f746ebb55a92
Drew Blaisdell <drew.blaisdell@gmail.com>
2016-06-21 14:03:17 -0400

    Use the host slug instead of name in the URL

--- app/components/containers/host-info.js
+++ app/components/containers/host-info.js
@@ -3,11 +3,9 @@ import { connect } from 'react-redux';
 
 // Internal dependencies
 import HostInfo from 'components/ui/host-info';
-import { getPath } from 'routes';
 
 export default connect(
 	( state, ownProps ) => ( {
-		hostName: ownProps.params.hostName,
-		backUrl: getPath( 'hosts' )
+		slug: ownProps.params.slug
 	} )
 )( HostInfo );
--- app/components/ui/hosts/index.js
+++ app/components/ui/hosts/index.js
@@ -2,7 +2,7 @@
 import i18n from 'i18n-calypso';
 import React from 'react';
 import withStyles from 'isomorphic-style-loader/lib/withStyles';
-import HostThumbnailContainer from 'components/containers/host-thumbnail';
+import HostThumbnail from 'components/ui/hosts/host-thumbnail';
 
 // Internal dependencies
 import { hosts } from 'lib/hosts';
@@ -15,7 +15,7 @@ class Hosts extends React.Component {
 				<h2 className={ styles.heading2 }>{ i18n.translate( 'Where would you like to create your blog?' ) }</h2>
 				<h3 className={ styles.heading3 }>{ i18n.translate( 'Here are a few powerful options that can be connected automatically to your domain using MagicDomains.' ) }</h3>
 				<ul className={ styles.hostThumbnailsList } >
-					{ hosts.map( host => <HostThumbnailContainer key={ host.name } { ...host } /> ) }
+					{ hosts.map( host => <HostThumbnail key={ host.name } { ...host } /> ) }
 				</ul>
 			</section>
 		);

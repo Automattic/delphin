15a51c01a0b4334e54d442bc2e039d83170cd29a
Drew Blaisdell <drew.blaisdell@gmail.com>
2016-06-21 13:53:04 -0400

    Remove padding from `NoMarginLayout`

--- app/components/ui/hosts/index.js
+++ app/components/ui/hosts/index.js
@@ -10,13 +10,15 @@ import styles from './styles.scss';
 
 class Hosts extends React.Component {
 	render() {
-		return <section>
-			<h2 className={ styles.heading2 }>{ i18n.translate( 'Where would you like to create your blog?' ) }</h2>
-			<h3 className={ styles.heading3 }>{ i18n.translate( 'Here are a few powerful options that can be connected automatically to your domain using MagicDomains.' ) }</h3>
-			<ul className={ styles.hostThumbnailsList } >
-				{ hosts.map( host => <HostThumbnailContainer key={ host.name } { ...host } /> ) }
-			</ul>
-		</section>;
+		return (
+			<section className={ styles.content }>
+				<h2 className={ styles.heading2 }>{ i18n.translate( 'Where would you like to create your blog?' ) }</h2>
+				<h3 className={ styles.heading3 }>{ i18n.translate( 'Here are a few powerful options that can be connected automatically to your domain using MagicDomains.' ) }</h3>
+				<ul className={ styles.hostThumbnailsList } >
+					{ hosts.map( host => <HostThumbnailContainer key={ host.name } { ...host } /> ) }
+				</ul>
+			</section>
+		);
 	}
 }
 

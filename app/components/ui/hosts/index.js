86dcf3a774eaaddf668b7e5ddb4513551b4ca178
Ben Dwyer <ben@scruffian.com>
2016-06-22 14:39:26 +0100

    Turn this into a stateless component

--- app/components/ui/hosts/index.js
+++ app/components/ui/hosts/index.js
@@ -8,22 +8,20 @@ import HostThumbnail from 'components/ui/hosts/host-thumbnail';
 import { hosts } from 'lib/hosts';
 import styles from './styles.scss';
 
-class Hosts extends React.Component {
-	render() {
-		return (
-			<section className={ styles.content }>
-				<h2 className={ styles.heading2 }>
-					{ i18n.translate( 'Where would you like to create your blog?' ) }
-				</h2>
-				<h3 className={ styles.heading3 }>
-					{ i18n.translate( 'Here are a few powerful options that can be connected automatically to your domain using MagicDomains.' ) }
-				</h3>
-				<ul className={ styles.hostThumbnailsList } >
-					{ hosts.map( host => <HostThumbnail key={ host.name } { ...host } /> ) }
-				</ul>
-			</section>
-		);
-	}
-}
+const Hosts = () => {
+	return (
+		<section className={ styles.content }>
+			<h2 className={ styles.heading2 }>
+				{ i18n.translate( 'Where would you like to create your blog?' ) }
+			</h2>
+			<h3 className={ styles.heading3 }>
+				{ i18n.translate( 'Here are a few powerful options that can be connected automatically to your domain using MagicDomains.' ) }
+			</h3>
+			<ul className={ styles.hostThumbnailsList } >
+				{ hosts.map( host => <HostThumbnail key={ host.name } { ...host } /> ) }
+			</ul>
+		</section>
+	);
+};
 
 export default withStyles( styles )( Hosts );

f9ef4193ac76d8ab24aac25c6b691e9c8c583c68
Drew Blaisdell <drew.blaisdell@gmail.com>
2016-07-21 14:57:06 -0400

    Move `redirect` action to its own module

--- app/components/containers/sunrise-confirm-domain.js
+++ app/components/containers/sunrise-confirm-domain.js
@@ -1,16 +1,15 @@
 // External dependencies
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
-import { push } from 'react-router-redux';
 
 // Internal dependencies
 import { getCheckout } from 'reducers/checkout/selectors';
-import { getPath } from 'routes';
+import { redirect } from 'actions/routes';
 import SunriseConfirmDomain from 'components/ui/sunrise-confirm-domain';
 
 export default connect(
 	state => ( { domain: getCheckout( state ).selectedDomain.domain } ),
 	dispatch => bindActionCreators( {
-		redirect: pathSlug => push( getPath( pathSlug ) )
+		redirect
 	}, dispatch )
 )( SunriseConfirmDomain );

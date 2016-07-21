5c2c683aadd1fc371f9bd102336bf4bea778e17f
Drew Blaisdell <drew.blaisdell@gmail.com>
2016-07-21 15:12:49 -0400

    Redirect the user to signup or contact information after submitting `SunriseConfirmDomain`

--- app/components/containers/sunrise-confirm-domain.js
+++ app/components/containers/sunrise-confirm-domain.js
@@ -4,11 +4,15 @@ import { connect } from 'react-redux';
 
 // Internal dependencies
 import { getCheckout } from 'reducers/checkout/selectors';
+import { isLoggedIn } from 'reducers/user/selectors';
 import { redirect } from 'actions/routes';
 import SunriseConfirmDomain from 'components/ui/sunrise-confirm-domain';
 
 export default connect(
-	state => ( { domain: getCheckout( state ).selectedDomain.domain } ),
+	state => ( {
+		domain: getCheckout( state ).selectedDomain.domain,
+		isLoggedIn: isLoggedIn( state )
+	} ),
 	dispatch => bindActionCreators( {
 		redirect
 	}, dispatch )

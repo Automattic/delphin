a789998e9d18d0ec238c537eb6ddfcd6d2393483
Drew Blaisdell <drew.blaisdell@gmail.com>
2016-07-21 14:36:14 -0400

    Update `SunriseConfirmDomain` to use stateful component definition
    We need to add methods to this in order to redirect if no domain is selected.

--- app/components/ui/sunrise-confirm-domain/index.js
+++ app/components/ui/sunrise-confirm-domain/index.js
@@ -11,50 +11,56 @@ import { getPath } from 'routes';
 import styles from './styles.scss';
 import SunriseStep from 'components/ui/sunrise-step';
 
-const SunriseConfirmDomain = ( { domain } ) => (
-	<SunriseStep>
-		<DocumentTitle title={ i18n.translate( 'We found it!' ) } />
-		<SunriseStep.Header>
-			<h1>
-				{ i18n.translate( 'We found it!' ) }
-			</h1>
-			<h2>
-				{ i18n.translate( 'That looks like a nice domain. Apply for ' +
-					'this domain to secure it for your blog.' ) }
-			</h2>
-		</SunriseStep.Header>
+class SunriseConfirmDomain extends React.Component {
+	render() {
+		const { domain } = this.props;
 
-		<SunriseStep.Form className={ styles.confirmDomainForm }>
-			<h3>
-				{ domain }
-			</h3>
-			<hr className={ styles.rule } />
-			<div className={ styles.priceTag }>
-				{ i18n.translate( '$220 application fee' ) }
-			</div>
-			<div className={ styles.renewalInfo }>
-				{ i18n.translate( 'Renews at $30 per year' ) }
-			</div>
-			<Button className={ styles.button }>
-				{ i18n.translate( 'Apply for this domain' ) }
-			</Button>
-			<div className={ styles.backNotice }>
-				<div>
-					{ i18n.translate( 'Not the domain you want?' ) }
-				</div>
-				<Link to={ getPath( 'home' ) }>
-					{ i18n.translate( 'Try a different domain' ) }
-				</Link>
-			</div>
-		</SunriseStep.Form>
+		return (
+			<SunriseStep>
+				<DocumentTitle title={ i18n.translate( 'We found it!' ) } />
+				<SunriseStep.Header>
+					<h1>
+						{ i18n.translate( 'We found it!' ) }
+					</h1>
+					<h2>
+						{ i18n.translate( 'That looks like a nice domain. Apply for ' +
+							'this domain to secure it for your blog.' ) }
+					</h2>
+				</SunriseStep.Header>
 
-		<SunriseStep.Footer>
-			{ i18n.translate( 'We may receive other applications for the domain you want. ' +
-					'If so, we\'ll notify all applicants and put the domain up for auction. ' +
-					'Applications are open until November 9.' ) }
-		</SunriseStep.Footer>
-	</SunriseStep>
-);
+				<SunriseStep.Form className={ styles.confirmDomainForm }>
+					<h3>
+						{ domain }
+					</h3>
+					<hr className={ styles.rule } />
+					<div className={ styles.priceTag }>
+						{ i18n.translate( '$220 application fee' ) }
+					</div>
+					<div className={ styles.renewalInfo }>
+						{ i18n.translate( 'Renews at $30 per year' ) }
+					</div>
+					<Button className={ styles.button }>
+						{ i18n.translate( 'Apply for this domain' ) }
+					</Button>
+					<div className={ styles.backNotice }>
+						<div>
+							{ i18n.translate( 'Not the domain you want?' ) }
+						</div>
+						<Link to={ getPath( 'home' ) }>
+							{ i18n.translate( 'Try a different domain' ) }
+						</Link>
+					</div>
+				</SunriseStep.Form>
+
+				<SunriseStep.Footer>
+					{ i18n.translate( 'We may receive other applications for the domain you want. ' +
+							'If so, we\'ll notify all applicants and put the domain up for auction. ' +
+							'Applications are open until November 9.' ) }
+				</SunriseStep.Footer>
+			</SunriseStep>
+		);
+	}
+}
 
 SunriseConfirmDomain.propTypes = {
 	domain: PropTypes.string.isRequired

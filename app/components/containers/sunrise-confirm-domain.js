47e3a4144931715f13c477d3053f681138e0f89e
Drew Blaisdell <drew.blaisdell@gmail.com>
2016-07-21 14:47:20 -0400

    Redirect from `SunriseConfirmDomain` to home if no domain is selected

--- app/components/containers/sunrise-confirm-domain.js
+++ app/components/containers/sunrise-confirm-domain.js
@@ -1,10 +1,16 @@
 // External dependencies
+import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
+import { push } from 'react-router-redux';
 
 // Internal dependencies
 import { getCheckout } from 'reducers/checkout/selectors';
+import { getPath } from 'routes';
 import SunriseConfirmDomain from 'components/ui/sunrise-confirm-domain';
 
 export default connect(
-	state => ( { domain: getCheckout( state ).selectedDomain.domain } )
+	state => ( { domain: getCheckout( state ).selectedDomain.domain } ),
+	dispatch => bindActionCreators( {
+		redirect: pathSlug => push( getPath( pathSlug ) )
+	}, dispatch )
 )( SunriseConfirmDomain );

1fa8113c9fa26afde2aef33b674f28434443c98e
Ben Dwyer <ben@scruffian.com>
2016-07-21 15:11:39 +0100

    Revert "Add `TrademarkVerification`"

--- app/components/ui/sunrise-confirm-domain/index.js
+++ app/components/ui/sunrise-confirm-domain/index.js
@@ -1,5 +1,4 @@
 // External dependencies
-import { bindHandlers } from 'react-bind-handlers';
 import i18n from 'i18n-calypso';
 import { Link } from 'react-router';
 import React, { PropTypes } from 'react';
@@ -12,66 +11,53 @@ import { getPath } from 'routes';
 import styles from './styles.scss';
 import SunriseStep from 'components/ui/sunrise-step';
 
-class SunriseConfirmDomain extends React.Component {
-	handleSubmit( event ) {
-		event.preventDefault();
+const SunriseConfirmDomain = ( { domain } ) => (
+	<SunriseStep>
+		<DocumentTitle title={ i18n.translate( 'We found it!' ) } />
+		<SunriseStep.Header>
+			<h1>
+				{ i18n.translate( 'We found it!' ) }
+			</h1>
+			<h2>
+				{ i18n.translate( 'That looks like a nice domain. Apply for ' +
+					'this domain to secure it for your blog.' ) }
+			</h2>
+		</SunriseStep.Header>
 
-		this.props.redirect( 'verifyTrademark' );
-	}
+		<SunriseStep.Form className={ styles.confirmDomainForm }>
+			<h3>
+				{ domain }
+			</h3>
+			<hr className={ styles.rule } />
+			<div className={ styles.priceTag }>
+				{ i18n.translate( '$220 application fee' ) }
+			</div>
+			<div className={ styles.renewalInfo }>
+				{ i18n.translate( 'Renews at $30 per year' ) }
+			</div>
+			<Button className={ styles.button }>
+				{ i18n.translate( 'Apply for this domain' ) }
+			</Button>
+			<div className={ styles.backNotice }>
+				<div>
+					{ i18n.translate( 'Not the domain you want?' ) }
+				</div>
+				<Link to={ getPath( 'home' ) }>
+					{ i18n.translate( 'Try a different domain' ) }
+				</Link>
+			</div>
+		</SunriseStep.Form>
 
-	render() {
-		const { domain } = this.props;
-
-		return (
-			<SunriseStep>
-				<DocumentTitle title={ i18n.translate( 'Confirm Your Domain' ) } />
-				<SunriseStep.Header>
-					<h1>
-						{ i18n.translate( 'We found it!' ) }
-					</h1>
-					<h2>
-						{ i18n.translate( 'That looks like a nice domain. Apply for ' +
-							'this domain to secure it for your blog.' ) }
-					</h2>
-				</SunriseStep.Header>
-
-				<SunriseStep.Form onSubmit={ this.handleSubmit } className={ styles.confirmDomainForm }>
-					<h3>
-						{ domain }
-					</h3>
-					<hr className={ styles.rule } />
-					<div className={ styles.priceTag }>
-						{ i18n.translate( '$220 application fee' ) }
-					</div>
-					<div className={ styles.renewalInfo }>
-						{ i18n.translate( 'Renews at $30 per year' ) }
-					</div>
-					<Button className={ styles.button }>
-						{ i18n.translate( 'Apply for this domain' ) }
-					</Button>
-					<div className={ styles.backNotice }>
-						<div>
-							{ i18n.translate( 'Not the domain you want?' ) }
-						</div>
-						<Link to={ getPath( 'home' ) }>
-							{ i18n.translate( 'Try a different domain' ) }
-						</Link>
-					</div>
-				</SunriseStep.Form>
-
-				<SunriseStep.Footer>
-					{ i18n.translate( 'We may receive other applications for the domain you want. ' +
-							'If so, we\'ll notify all applicants and put the domain up for auction. ' +
-							'Applications are open until November 9.' ) }
-				</SunriseStep.Footer>
-			</SunriseStep>
-		);
-	}
-}
+		<SunriseStep.Footer>
+			{ i18n.translate( 'We may receive other applications for the domain you want. ' +
+					'If so, we\'ll notify all applicants and put the domain up for auction. ' +
+					'Applications are open until November 9.' ) }
+		</SunriseStep.Footer>
+	</SunriseStep>
+);
 
 SunriseConfirmDomain.propTypes = {
-	domain: PropTypes.string.isRequired,
-	redirect: PropTypes.func.isRequired
+	domain: PropTypes.string.isRequired
 };
 
-export default withStyles( styles )( bindHandlers( SunriseConfirmDomain ) );
+export default withStyles( styles )( SunriseConfirmDomain );

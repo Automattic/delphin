9b5a519e1decf1a14cd5bc5afa3d35d3c59671b3
Tugdual de Kerviler <dekervit@gmail.com>
2016-07-22 11:46:59 +0200

    Update domain selector in confirmDomain

--- app/components/containers/sunrise-confirm-domain.js
+++ app/components/containers/sunrise-confirm-domain.js
@@ -3,14 +3,14 @@ import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
 
 // Internal dependencies
-import { getCheckout } from 'reducers/checkout/selectors';
+import { getSelectedDomain } from 'reducers/checkout/selectors';
 import { isLoggedIn } from 'reducers/user/selectors';
 import { redirect } from 'actions/routes';
 import SunriseConfirmDomain from 'components/ui/sunrise-confirm-domain';
 
 export default connect(
 	state => ( {
-		domain: getCheckout( state ).selectedDomain.domain,
+		domain: getSelectedDomain( state ).domain,
 		isLoggedIn: isLoggedIn( state )
 	} ),
 	dispatch => bindActionCreators( {

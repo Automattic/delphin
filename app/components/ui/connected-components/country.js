7b36403f6627607137d28323cf8724709890fb95
Tugdual de Kerviler <dekervit@gmail.com>
2016-07-22 17:46:29 +0200

    Add propTypes to connected Country component

--- app/components/ui/connected-components/country.js
+++ app/components/ui/connected-components/country.js
@@ -1,20 +1,18 @@
 // External dependencies
 import { connect } from 'react-redux';
-import React from 'react';
+import React, { PropTypes } from 'react';
 
 // Internal dependencies
 import Country from 'components/ui/form/country';
 import { fetchCountries } from 'actions/territories';
 import { getCountries } from 'reducers/territories/selectors';
 
-export default connect(
-	state => ( {
-		countries: getCountries( state )
-	} ),
-	{
-		fetchCountries
-	}
-)( React.createClass( {
+const CountryWrapper = React.createClass( {
+	propTypes: {
+		countries: PropTypes.object.isRequired,
+		fetchCountries: PropTypes.func.isRequired
+	},
+
 	isDataLoading() {
 		return ! this.props.countries.isRequesting && ! this.props.countries.hasLoadedFromServer;
 	},
@@ -28,4 +26,13 @@ export default connect(
 	render() {
 		return <Country disabled={ this.isDataLoading() } { ...this.props }/>;
 	}
-} ) );
+} );
+
+export default connect(
+	state => ( {
+		countries: getCountries( state )
+	} ),
+	{
+		fetchCountries
+	}
+)( CountryWrapper );

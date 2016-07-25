df4e23f6516e9c059e26a37dd8426349eea992b9
Drew Blaisdell <drew.blaisdell@gmail.com>
2016-07-25 14:05:57 -0400

    Update `state.territories.countries` reducer to use `createRequestReducer`

--- app/reducers/territories/countries.js
+++ app/reducers/territories/countries.js
@@ -1,34 +1,13 @@
 // Internal dependencies
+import { createRequestReducer } from 'lib/create-request-reducer';
 import {
 	COUNTRIES_FETCH,
 	COUNTRIES_FETCH_COMPLETE,
 	COUNTRIES_FETCH_ERROR
 } from 'reducers/action-types';
 
-const initialState = {
-	isRequesting: false,
-	hasLoadedFromServer: false,
-	data: null
-};
-
-export const countries = ( state = initialState, action ) => {
-	const { type, data } = action;
-
-	switch ( type ) {
-		case COUNTRIES_FETCH:
-			return Object.assign( {}, state, { isRequesting: true } );
-		case COUNTRIES_FETCH_COMPLETE:
-			return Object.assign( {}, state, {
-				isRequesting: false,
-				hasLoadedFromServer: true,
-				data
-			} );
-		case COUNTRIES_FETCH_ERROR:
-			return Object.assign( {}, state, {
-				isRequesting: false
-			} );
-		default:
-			return state;
-	}
-};
-
+export const countries = createRequestReducer( {
+	loading: COUNTRIES_FETCH,
+	success: COUNTRIES_FETCH_COMPLETE,
+	fail: COUNTRIES_FETCH_ERROR
+} );

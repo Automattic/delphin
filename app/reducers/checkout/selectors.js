2bb25f2b121b9dd157d9ae8191193dbb7599134c
Drew Blaisdell <drew.blaisdell@gmail.com>
2016-07-29 14:12:44 -0400

    Display the total cost of a domain + application on `Checkout`

--- app/reducers/checkout/selectors.js
+++ app/reducers/checkout/selectors.js
@@ -1,5 +1,6 @@
 export const getCheckout = state => state.checkout;
 export const getSelectedDomain = state => getCheckout( state ).selectedDomain;
+export const getSelectedDomainPrice = state => getCheckout( state ).selectedDomainPrice;
 export const isPurchasing = state => [ 'paygateConfiguration', 'paygateToken', 'transaction' ]
 	.map( requestName => getCheckout( state )[ requestName ] )
 	.some( request => request.isRequesting );

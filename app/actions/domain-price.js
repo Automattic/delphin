5b52fe05fa924cfb70745141676734f1b1e73401
Drew Blaisdell <drew.blaisdell@gmail.com>
2016-07-29 14:23:07 -0400

    Removed unused `clearDomainPrice` action

--- app/actions/domain-price.js
+++ app/actions/domain-price.js
@@ -2,29 +2,13 @@
 import { addNotice } from 'actions/notices';
 import {
 	WPCOM_REQUEST,
-	DOMAIN_PRICE_CLEAR,
 	DOMAIN_PRICE_FETCH,
 	DOMAIN_PRICE_FETCH_COMPLETE,
 	DOMAIN_PRICE_FETCH_FAIL
 } from 'reducers/action-types';
 import { withTld } from 'lib/domains';
 
-/**
- * Returns an action object to be used in signalling that domain PRICE have been cleared.
- *
- * @returns {Object} the corresponding action object
- */
-export function clearDomainPrice() {
-	return {
-		type: DOMAIN_PRICE_CLEAR
-	};
-}
-
 export function fetchDomainPrice( domainQuery = '' ) {
-	if ( domainQuery.trim() === '' ) {
-		return clearDomainPrice();
-	}
-
 	const queryWithTld = withTld( domainQuery );
 
 	return {

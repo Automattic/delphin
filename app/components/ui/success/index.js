9cda314368078affe8e126462bec5d37665d1f2e
Ran <ran@a8c.com>
2016-07-31 17:21:59 +0300

    Success screen: fix linting errors (single quotes for strings)

--- app/components/ui/success/index.js
+++ app/components/ui/success/index.js
@@ -30,7 +30,7 @@ const Success = ( { domain, email } ) => (
 		<div className={ styles.content }>
 			<div className={ styles.highlight }>
 				<h3>
-					{ i18n.translate( "What happens now?" ) }
+					{ i18n.translate( 'What happens now?' ) }
 				</h3>
 
 				<p>
@@ -45,15 +45,15 @@ const Success = ( { domain, email } ) => (
 
 			<div className={ styles.text }>
 				<p>
-					{ i18n.translate( "If no other requests are received for this domain, you will be granted the domain shortly after November 9." ) }
+					{ i18n.translate( 'If no other requests are received for this domain, you will be granted the domain shortly after November 9.' ) }
 				</p>
 
 				<p>
-					{ i18n.translate( "If multiple requests are received for this domain, you will be able to bid for it in an auction, between November 14 and November 17." ) }
+					{ i18n.translate( 'If multiple requests are received for this domain, you will be able to bid for it in an auction, between November 14 and November 17.' ) }
 				</p>
 
 				<p>
-					{ i18n.translate( "Please set up an account with our auction partner, NameJet, so you will be ready in case your domain goes to auction." ) }
+					{ i18n.translate( 'Please set up an account with our auction partner, NameJet, so you will be ready in case your domain goes to auction.' ) }
 				</p>
 
 				<a

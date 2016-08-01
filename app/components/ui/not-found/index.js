6470a9dd2167657920aa38932ffeb5fa9e0499cd
Drew Blaisdell <drew.blaisdell@gmail.com>
2016-08-01 15:12:31 -0400

    Record a page view when users visit the 404 page

--- app/components/ui/not-found/index.js
+++ app/components/ui/not-found/index.js
@@ -2,8 +2,11 @@
 import i18n from 'i18n-calypso';
 import React from 'react';
 
-export default function NotFound() {
-	return (
-		<div>{ i18n.translate( '404 Not Found' ) }</div>
-	);
-}
+// Internal dependencies
+import withPageView from 'lib/analytics/with-page-view';
+
+const NotFound = () => (
+	<div>{ i18n.translate( '404 Not Found' ) }</div>
+);
+
+export default withPageView( NotFound, '404' );

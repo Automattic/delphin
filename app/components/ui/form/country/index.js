e98a0c5e768479f9dd399db7231b0ead8345eac6
Ben Dwyer <ben@scruffian.com>
2016-08-02 15:14:12 +0100

    Add a FormSelect component

--- app/components/ui/form/country/index.js
+++ app/components/ui/form/country/index.js
@@ -3,10 +3,11 @@ import i18n from 'i18n-calypso';
 import React, { PropTypes } from 'react';
 
 // Internal dependencies
+import FormSelect from 'components/ui/form/select';
 import { removeInvalidInputProps } from 'lib/form';
 
 const Country = ( { className, disabled, field, countries } ) => (
-	<select
+	<FormSelect
 		{ ...removeInvalidInputProps( field ) }
 		disabled={ disabled }
 		className={ className }>
@@ -17,7 +18,7 @@ const Country = ( { className, disabled, field, countries } ) => (
 				? <option value={ country.code } key={ country.code }>{ country.name }</option>
 				: <option value=" " key={ index } disabled />
 		) ) }
-	</select>
+	</FormSelect>
 );
 
 Country.propTypes = {

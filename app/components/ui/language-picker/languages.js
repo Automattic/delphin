3c6b167c4cbe97e220bd01b2c8fac16d40d4d328
Drew Blaisdell <drew.blaisdell@gmail.com>
2016-08-03 16:41:01 -0400

    Add `LanguagePicker`

---
+++ app/components/ui/language-picker/languages.js
@@ -0,0 +1,58 @@
+export default [
+	{
+		locale: 'en',
+		name: 'English'
+	},
+	{
+		locale: 'es',
+		name: 'Español'
+	},
+	{
+		locale: 'de',
+		name: 'Deutsch'
+	},
+	{
+		locale: 'it',
+		name: 'Italiano'
+	},
+	{
+		locale: 'he',
+		name: 'עברית'
+	},
+	{
+		locale: 'ja',
+		name: '日本語'
+	},
+	{
+		locale: 'pt-br',
+		name: 'Português do Brasil'
+	},
+	{
+		locale: 'nl',
+		name: 'Nederlands'
+	},
+	{
+		locale: 'ko',
+		name: '한국어'
+	},
+	{
+		locale: 'tr',
+		name: 'Türkçe'
+	},
+	{
+		locale: 'id',
+		name: 'Bahasa Indonesia'
+	},
+	{
+		locale: 'zh-cn',
+		name: '中文(简体)'
+	},
+	{
+		locale: 'ar',
+		name: 'العربية'
+	},
+	{
+		locale: 'sv',
+		name: 'Svenska'
+	}
+];
--- app/components/ui/menu/index.js
+++ app/components/ui/menu/index.js
@@ -4,6 +4,7 @@ import React, { PropTypes } from 'react';
 import withStyles from 'isomorphic-style-loader/lib/withStyles';
 
 // Internal dependencies
+import LanguagePicker from 'components/containers/language-picker';
 import styles from './styles.scss';
 import TrackingLink from 'components/containers/tracking-link';
 
@@ -13,6 +14,7 @@ const Menu = () => {
 			<TrackingLink eventName="delphin_footer_link_click" eventParams={ { link_clicked: 'support' } } className={ styles.link } to="mailto:help@get.blog">{ i18n.translate( 'Support' ) }</TrackingLink>
 			<TrackingLink eventName="delphin_footer_link_click" eventParams={ { link_clicked: 'term_of_use' } } className={ styles.link } to="https://wordpress.com/automattic-domain-name-registration-agreement/">{ i18n.translate( 'Terms of Use' ) }</TrackingLink>
 			<TrackingLink eventName="delphin_footer_link_click" eventParams={ { link_clicked: 'wordpress' } } className={ styles.link } to="https://wordpress.com">{ i18n.translate( 'A WordPress.com Service' ) }</TrackingLink>
+			<LanguagePicker />
 		</menu>
 	);
 };

dfdfb163dd7127156d95c6152448812eb3a899d8
Ben Dwyer <ben@scruffian.com>
2016-08-03 16:06:01 +0100

    Add styles to validation messages on the home page

--- app/components/ui/domain-input/index.js
+++ app/components/ui/domain-input/index.js
@@ -4,16 +4,23 @@ import React, { PropTypes } from 'react';
 import withStyles from 'isomorphic-style-loader/lib/withStyles';
 
 // Internal dependencies
+import Input from 'components/ui/form/input';
 import { removeInvalidInputProps } from 'lib/form';
 import styles from './styles.scss';
 
 const DomainInput = props => {
-	const { className, ...inputProps } = props;
+	const { className, ...inputProps, field } = props,
+		isInvalid = field.touched && field.error,
+		inputClassName = classNames( className, styles.domainInput, {
+			[ styles.hasError ]: isInvalid
+		} );
 
 	return (
-		<div className={ classNames( className, styles.domainInput ) }>
-			<input
-				className={ styles.input }
+		<div className={ inputClassName }>
+			<Input
+				className={ styles.inputContainer }
+				inputClassName={ styles.input }
+				gridIconSize={ 32 }
 				{ ...removeInvalidInputProps( inputProps ) }
 			/>
 			<div className={ styles.tldContainer }>
@@ -24,7 +31,8 @@ const DomainInput = props => {
 };
 
 DomainInput.propTypes = {
-	className: PropTypes.string
+	className: PropTypes.string,
+	field: PropTypes.object
 };
 
 export default withStyles( styles )( DomainInput );

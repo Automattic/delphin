d2b7d9d8c0a2630dc2c3813b88d0210a1c2d830e
Ben Dwyer <ben@scruffian.com>
2016-08-03 16:13:15 +0100

    Domain validation: Domains ending with .blog should ignore the .blog part

--- app/components/containers/sunrise-home.js
+++ app/components/containers/sunrise-home.js
@@ -14,31 +14,36 @@ import { selectDomain } from 'actions/domain-search';
 import SunriseHome from 'components/ui/sunrise-home';
 
 const validate = values => {
-	if ( ! values.query.trim() ) {
+	let stringToValidate = values.query;
+	if ( ! stringToValidate.trim() ) {
 		return { query: i18n.translate( 'Please enter a domain name' ) };
 	}
 
-	if ( values.query.length < 4 ) {
+	if ( stringToValidate.indexOf( '.blog' ) > -1 ) {
+		stringToValidate = stringToValidate.replace( /\.blog/gi, '' );
+	}
+
+	if ( stringToValidate.length < 4 ) {
 		return { query: i18n.translate( 'Choose a longer domain, at least four characters.' ) };
 	}
 
-	if ( values.query.length > 63 ) {
+	if ( stringToValidate.length > 63 ) {
 		return { query: i18n.translate( 'Choose a shorter domain, up to 63 characters (not including the ".blog" part)' ) };
 	}
 
-	if ( values.query.indexOf( '-' ) === 0 ) {
+	if ( stringToValidate.indexOf( '-' ) === 0 ) {
 		return { query: i18n.translate( 'Don’t use a "-" (dash) as the first character in your domain.' ) };
 	}
 
-	if ( values.query.indexOf( '-' ) === values.query.length - 1 ) {
+	if ( stringToValidate.indexOf( '-' ) === stringToValidate.length - 1 ) {
 		return { query: i18n.translate( 'Don’t use a "-" (dash) as the last character in your domain.' ) };
 	}
 
-	if ( values.query.indexOf( '.' ) > -1 ) {
+	if ( stringToValidate.indexOf( '.' ) > -1 ) {
 		return { query: i18n.translate( 'Don’t use a "." (period) in your domain.' ) };
 	}
 
-	if ( ! isValidDomain( values.query + '.blog' ) ) {
+	if ( ! isValidDomain( stringToValidate + '.blog' ) ) {
 		return { query: i18n.translate( 'Use only lowercase letters, numbers, and dashes (a to z, 0 to 9, and -). Spaces or other characters are not supported.' ) };
 	}
 

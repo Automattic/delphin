ef3602d1f84895f2af13acf30652cf7a625dc8f8
Drew Blaisdell <drew.blaisdell@gmail.com>
2016-08-05 13:29:47 -0400

    Support external links in `TrackingLink`

--- app/components/ui/tracking-link/index.js
+++ app/components/ui/tracking-link/index.js
@@ -2,10 +2,14 @@
 import React, { PropTypes } from 'react';
 import { Link } from 'react-router';
 import omit from 'lodash/omit';
+import startsWith from 'lodash/startsWith';
 
 const TrackingLink = props => {
 	const { onClick } = props;
 	const newProps = omit( props, [ 'eventName', 'eventParams', 'trackEvent' ] );
+	const isExternalLink = startsWith( props.to, 'https://' ) ||
+		startsWith( props.to, 'http://' ) ||
+		startsWith( props.to, 'mailto:' );
 
 	newProps.onClick = ( ...args ) => {
 		// track event, it's already bound with eventName on the container
@@ -15,6 +19,20 @@ const TrackingLink = props => {
 			return onClick( ...args );
 		}
 	};
+
+	if ( isExternalLink ) {
+		const href = newProps.to;
+
+		if ( ! startsWith( href, 'mailto:' ) ) {
+			newProps.target = '_blank';
+		}
+
+		return (
+			<a href={ href } { ...omit( newProps, 'to' ) }>
+				{ props.children }
+			</a>
+		);
+	}
 
 	return (
 		<Link { ...newProps }>
@@ -26,6 +44,7 @@ const TrackingLink = props => {
 TrackingLink.propTypes = {
 	children: PropTypes.node,
 	onClick: PropTypes.func,
+	to: PropTypes.string,
 	trackEvent: PropTypes.func.isRequired
 };
 

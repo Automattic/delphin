5c9cae770e509495e71168e861110eb0ac2a4ef3
Fabiana Simões <fabianapsimoes@gmail.com>
2016-08-05 17:12:50 +0200

    Support: update support links to use new config key

--- app/components/ui/menu/index.js
+++ app/components/ui/menu/index.js
@@ -4,13 +4,14 @@ import React, { PropTypes } from 'react';
 import withStyles from 'isomorphic-style-loader/lib/withStyles';
 
 // Internal dependencies
+import config from 'config';
 import styles from './styles.scss';
 import TrackingLink from 'components/containers/tracking-link';
 
 const Menu = () => {
 	return (
 		<menu className={ styles.menu }>
-			<TrackingLink eventName="delphin_footer_link_click" eventParams={ { link_clicked: 'support' } } className={ styles.link } to="mailto:help@get.blog">{ i18n.translate( 'Support' ) }</TrackingLink>
+			<TrackingLink eventName="delphin_footer_link_click" eventParams={ { link_clicked: 'support' } } className={ styles.link } to={ config( 'support_link' ) }>{ i18n.translate( 'Support' ) }</TrackingLink>
 			<TrackingLink eventName="delphin_footer_link_click" eventParams={ { link_clicked: 'term_of_use' } } className={ styles.link } to="https://wordpress.com/automattic-domain-name-registration-agreement/">{ i18n.translate( 'Terms of Use' ) }</TrackingLink>
 			<TrackingLink eventName="delphin_footer_link_click" eventParams={ { link_clicked: 'wordpress' } } className={ styles.link } to="https://wordpress.com">{ i18n.translate( 'A WordPress.com Service' ) }</TrackingLink>
 		</menu>

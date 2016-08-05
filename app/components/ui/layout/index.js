e508447a4cd86c0622df82916931297d903af8cb
Ben Dwyer <ben@scruffian.com>
2016-08-05 09:50:05 +0100

    add notices container back

--- app/components/ui/layout/index.js
+++ app/components/ui/layout/index.js
@@ -3,12 +3,15 @@ import React, { PropTypes } from 'react';
 import withStyles from 'isomorphic-style-loader/lib/withStyles';
 
 // Internal dependencies
+import NoticesContainer from 'components/containers/notices';
 import styles from './styles.scss';
 
 const Layout = ( { children } ) => {
 	return (
 		<div className={ styles.layout }>
 			{ children }
+
+			<NoticesContainer />
 		</div>
 	);
 };

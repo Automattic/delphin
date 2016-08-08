be034af4139e5dbf3148019dfdcea7f9a02af8d5
Drew Blaisdell <drew.blaisdell@gmail.com>
2016-08-08 16:20:38 -0400

    Set the locale cookie when switching languages

--- client/switch-locale.js
+++ client/switch-locale.js
@@ -1,8 +1,11 @@
-
+// External dependencies
 import request from 'superagent';
 import i18n from 'i18n-calypso';
 import debugFactory from 'debug';
 const debug = debugFactory( 'delphin:i18n' );
+
+// Internal dependencies
+import { setLocaleCookie } from './locale-cookie';
 
 function languageFileUrl( localeSlug ) {
 	var protocol = typeof window === 'undefined' ? 'https://' : '//', // use a protocol-relative path in the browser
@@ -11,6 +14,7 @@ function languageFileUrl( localeSlug ) {
 }
 
 function switchLocale( localeSlug ) {
+	setLocaleCookie( localeSlug );
 	request.get( languageFileUrl( localeSlug ) ).end( function( error, response ) {
 		if ( error ) {
 			debug( 'Encountered an error loading locale file for ' + localeSlug + '. Falling back to English.' );

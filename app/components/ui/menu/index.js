1ba1b7974474d8dee0ed1f6524f4c7452055a794
Brie Anne Demkiw <brie@automattic.com>
2016-08-10 07:42:53 -0500

    Footer: Add learn more link to dotblog.wordpress.com.

--- app/components/ui/menu/index.js
+++ app/components/ui/menu/index.js
@@ -11,6 +11,7 @@ import TrackingLink from 'components/containers/tracking-link';
 const Menu = () => {
 	return (
 		<menu className={ styles.menu }>
+			<TrackingLink eventName="delphin_footer_link_click" eventParams={ { link_clicked: 'learn_more' } } className={ styles.link } to="https://dotblog.wordpress.com">{ i18n.translate( 'Learn More' ) }</TrackingLink>
 			<TrackingLink eventName="delphin_footer_link_click" eventParams={ { link_clicked: 'support' } } className={ styles.link } to={ config( 'support_link' ) }>{ i18n.translate( 'Support' ) }</TrackingLink>
 			<TrackingLink eventName="delphin_footer_link_click" eventParams={ { link_clicked: 'term_of_use' } } className={ styles.link } to="https://wordpress.com/automattic-domain-name-registration-agreement/">{ i18n.translate( 'Terms of Use' ) }</TrackingLink>
 			<TrackingLink eventName="delphin_footer_link_click" eventParams={ { link_clicked: 'wordpress' } } className={ styles.link } to="https://wordpress.com">{ i18n.translate( 'A WordPress.com Service' ) }</TrackingLink>

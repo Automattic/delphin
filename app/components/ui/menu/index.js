a4f9334252fde5d2b4082ee8f4da3cb451a8233d
Brie Anne Demkiw <brie@automattic.com>
2016-08-10 18:05:26 -0500

    Footer: Replace terms link with privacy link.

--- app/components/ui/menu/index.js
+++ app/components/ui/menu/index.js
@@ -13,7 +13,7 @@ const Menu = () => {
 		<menu className={ styles.menu }>
 			<TrackingLink eventName="delphin_footer_link_click" eventParams={ { link_clicked: 'learn_more' } } className={ styles.link } to="https://dotblog.wordpress.com">{ i18n.translate( 'Learn More' ) }</TrackingLink>
 			<TrackingLink eventName="delphin_footer_link_click" eventParams={ { link_clicked: 'support' } } className={ styles.link } to={ config( 'support_link' ) }>{ i18n.translate( 'Support' ) }</TrackingLink>
-			<TrackingLink eventName="delphin_footer_link_click" eventParams={ { link_clicked: 'term_of_use' } } className={ styles.link } to="https://wordpress.com/automattic-domain-name-registration-agreement/">{ i18n.translate( 'Terms of Use' ) }</TrackingLink>
+			<TrackingLink eventName="delphin_footer_link_click" eventParams={ { link_clicked: 'term_of_use' } } className={ styles.link } to="https://automattic.com/privacy/">{ i18n.translate( 'Privacy Policy' ) }</TrackingLink>
 			<TrackingLink eventName="delphin_footer_link_click" eventParams={ { link_clicked: 'wordpress' } } className={ styles.link } to="https://wordpress.com">{ i18n.translate( 'A WordPress.com Service' ) }</TrackingLink>
 		</menu>
 	);

b9e9cfdbff0fabae1f1de57fffa510b64d23593e
Drew Blaisdell <drew.blaisdell@gmail.com>
2016-08-12 12:31:40 -0400

    Remove `noticeArea` from `Form`
    This isn't used anymore.

--- app/components/ui/form/index.js
+++ app/components/ui/form/index.js
@@ -5,10 +5,8 @@ import withStyles from 'isomorphic-style-loader/lib/withStyles';
 // Internal dependencies
 import styles from './styles.scss';
 
-const Form = ( { onSubmit, noticeArea, fieldArea, submitArea } ) => (
+const Form = ( { onSubmit, fieldArea, submitArea } ) => (
 	<form onSubmit={ onSubmit } className={ styles.form }>
-		{ noticeArea && <div className={ styles.noticeArea }>{ noticeArea }</div> }
-
 		<div className={ styles.fieldArea }>
 			{ fieldArea }
 		</div>
@@ -21,7 +19,6 @@ const Form = ( { onSubmit, noticeArea, fieldArea, submitArea } ) => (
 
 Form.propTypes = {
 	fieldArea: PropTypes.element.isRequired,
-	noticeArea: PropTypes.node,
 	onSubmit: PropTypes.func.isRequired,
 	submitArea: PropTypes.element.isRequired
 };

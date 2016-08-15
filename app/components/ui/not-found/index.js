86b2cefb79afc88aafaed13b5947a54b2554e3ea
Dan Hauk <hauk.dan@gmail.com>
2016-08-15 13:53:02 -0400

    Add new structure and styles for 404 page

--- app/components/ui/not-found/index.js
+++ app/components/ui/not-found/index.js
@@ -1,12 +1,26 @@
 // External dependencies
 import i18n from 'i18n-calypso';
 import React from 'react';
+import withStyles from 'isomorphic-style-loader/lib/withStyles';
 
 // Internal dependencies
+import Footer from 'components/ui/footer';
+import Header from 'components/ui/header';
 import withPageView from 'lib/analytics/with-page-view';
+import styles from './styles.scss';
 
 const NotFound = () => (
-	<div>{ i18n.translate( '404 Not Found' ) }</div>
+	<div>
+		<div className={ styles.content }>
+			<Header/>
+			<div className={ styles.fourOhFour }>
+				<h1>{ i18n.translate( 'Page not found' ) }</h1>
+				<h2>{ i18n.translate( 'Sorry, the page you were looking for doesn\'t exist or has been moved.' ) }</h2>
+			</div>
+		</div>
+
+		<Footer hasBorder />
+	</div>
 );
 
-export default withPageView( NotFound, '404' );
+export default withStyles( styles )( withPageView( NotFound, '404' ) );

3312b9d4e4297ddd5a0f048df2f87da1248ec720
Stéphane Thomas <email@stephanethomas.com>
2016-08-17 18:15:49 +0200

    Fix syntax error in life cycle method of Scroll To Top decorator

--- app/components/containers/scroll-to-top.js
+++ app/components/containers/scroll-to-top.js
@@ -2,14 +2,14 @@
 import React from 'react';
 
 const scrollToTop = WrappedComponent => class Scroller extends React.Component {
-	componetDidMount() {
+	componentDidMount() {
 		// because that method invoked only on the client, no additional check required
 		// https://facebook.github.io/react/docs/component-specs.html#mounting-componentdidmount
 		window.scrollTo( 0, 0 );
 	}
 
 	render() {
-		return <WrappedComponent {...this.props}></WrappedComponent>;
+		return <WrappedComponent {...this.props} />;
 	}
 };
 

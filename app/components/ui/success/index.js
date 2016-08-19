1f5d1056cab742976125723a1c213a57161ec49a
Michael Shelton <mike.shelton@automattic.com>
2016-08-19 08:57:27 -0400

    Fixing tranlation strings

--- app/components/ui/success/index.js
+++ app/components/ui/success/index.js
@@ -64,7 +64,8 @@ class Success extends React.Component {
 						</p>
 
 						<p>
-							{ i18n.translate( 'If multiple requests are received for this domain, you will be able to bid for it in an auction, between November 14 and November 17. If your domain goes to auction and you don\'t win, your payment will be refunded.' ) }
+							{ i18n.translate( 'If multiple requests are received for this domain, you will be able to bid for it in an auction, between November 14 and November 17.' ) }&nbsp;
+							{ i18n.translate( 'Your payment will be refunded if your domain goes to auction and you don\'t win.' ) }
 						</p>
 
 						<p>

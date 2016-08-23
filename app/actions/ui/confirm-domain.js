a2ac089c7fda15986d49f0c07a7ca207cb9c8ab8
Drew Blaisdell <drew.blaisdell@gmail.com>
2016-08-23 17:16:08 -0400

    Replace `state.ui.confirmDomain` with `state.ui.toggle`

--- app/actions/ui/confirm-domain.js
+++
@@ -1,6 +0,0 @@
-// Internal dependencies
-import { CONFIRM_DOMAIN_MORE_INFORMATION_SHOW } from 'reducers/action-types';
-
-export const showConfirmDomainMoreInformation = () => ( {
-	type: CONFIRM_DOMAIN_MORE_INFORMATION_SHOW
-} );

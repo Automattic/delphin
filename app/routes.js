3cd72ac57938c8a87185d431eacd0acc50fd82eb
Drew Blaisdell <drew.blaisdell@gmail.com>
2016-08-29 12:21:51 -0400

    Update condition to mount in-progress components if no environment is set

--- app/routes.js
+++ app/routes.js
@@ -130,7 +130,7 @@ let publicRoutes = [
 	}
 ];
 
-if ( process.env.NODE_ENV === 'development' ) {
+if ( ! process.env.NODE_ENV || process.env.NODE_ENV === 'development' ) {
 	const SearchContainer = require( 'components/containers/search' ).default;
 
 	publicRoutes.push( {

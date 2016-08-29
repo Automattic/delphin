c7325023f219ebde5af51b27f86ef2dac012ad8e
Tugdual de Kerviler <dekervit@gmail.com>
2016-08-29 14:05:55 +0200

    Add validation to the Supscription form and improve design (make it responsive)

--- app/components/containers/learn-more.js
+++ app/components/containers/learn-more.js
@@ -1,14 +1,21 @@
 // External dependencies
+import isEmpty from 'lodash/isEmpty';
+import omitBy from 'lodash/omitBy';
 import { reduxForm, reset } from 'redux-form';
 
 // Internal dependencies
 import { addNotice } from 'actions/notices';
+import { getAsyncValidateFunction } from 'lib/form';
+import { validateDomain } from 'lib/domains';
 import LearnMore from 'components/ui/learn-more';
+
+const validate = ( values ) => omitBy( { domain: validateDomain( values.domain ) }, isEmpty );
 
 export default reduxForm(
 	{
 		form: 'learn-more',
 		fields: [ 'domain', 'email' ],
+		asyncValidate: getAsyncValidateFunction( validate )
 	},
 	undefined,
 	{

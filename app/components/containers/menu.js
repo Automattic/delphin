6c9a03bf976372fa279ed0819d55e3e4588a4879
Stéphane Thomas <email@stephanethomas.com>
2016-09-08 19:31:53 +0200

    Add login and logout to main menu
    They will only be displayed in development for now.

--- app/components/containers/menu.js
+++ app/components/containers/menu.js
@@ -1,18 +1,21 @@
 // External dependencies
+import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
 
 // Internal dependencies
 import Menu from 'components/ui/menu';
 import { isLoggedIn } from 'reducers/user/selectors';
 import { logoutUser } from 'actions/user';
+import { recordTracksEvent, withAnalytics } from 'actions/analytics';
 
 export default connect(
 	state => ( {
 		isLoggedIn: isLoggedIn( state )
 	} ),
-	dispatch => ( {
-		logoutUser() {
-			return dispatch( logoutUser() );
-		}
-	} )
+	dispatch => bindActionCreators( {
+		logoutUser: withAnalytics(
+			recordTracksEvent( 'delphin_footer_link_click', { link_clicked: 'logout' } ),
+			logoutUser
+		)
+	}, dispatch )
 )( Menu );

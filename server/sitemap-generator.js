ccc631fcad572f5847faba6e855f53f5afa1328e
Stéphane Thomas <email@stephanethomas.com>
2016-09-27 20:30:16 +0200

    Update sitemap to only list specific routes
    This makes sure new routes won't be indexed by search engines until they are explicitly whitelisted.

--- server/sitemap-generator.js
+++ server/sitemap-generator.js
@@ -6,28 +6,28 @@ import SiteMap from 'react-router-sitemap';
 import config from 'app/config';
 import { routes } from 'app/routes';
 
-// exclude some paths
+// Builds a list of non-English locales to use in the regular expressions below
+const locales = config( 'languages' ).filter( language => {
+	return language.langSlug !== 'en';
+} ).map( language => {
+	return language.langSlug;
+} ).join( '|' );
+
+// Includes only very specific routes in the sitemap
 const filterConfig = {
-	isValid: false,
+	isValid: true,
 	rules: [
-		/\/contact-information/,
-		/\/confirm-domain/,
-		/\/verify/,
-		/\/checkout/,
-		/\/checkout-review/,
-		/\/testnest/,
-		/\/sign-in-with-email/,
-		/\/sign-up-with-email/,
-		/\/success/,
-		/\/log-in/,
-		/\/signup/,
-		/\/\*/
+		new RegExp( `^\/(${ locales })?$` ),
+		new RegExp( `^(?:\/(${ locales }))?\/learn-more$` ),
+		new RegExp( `^(?:\/(${ locales }))?\/search$` )
 	]
 };
 
-export default () => (
+export default () => {
+	console.log( 'sitemap.xml written' );
+
 	new SiteMap( routes )
 		.filterPaths( filterConfig )
 		.build( 'https://' + config( 'hostname' ) )
-		.save( path.join( __dirname, '..', 'public', 'static', 'sitemap.xml' ) )
-);
+		.save( path.join( __dirname, '..', 'public', 'static', 'sitemap.xml' ) );
+};

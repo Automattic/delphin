2debe85e3e0102f73a9b142de1117ebad629fe38
Drew Blaisdell <drew.blaisdell@gmail.com>
2016-09-27 14:52:24 -0700

    Display a pulsing dot when sections are loading

--- app/sections/index.js
+++ app/sections/index.js
@@ -1,3 +1,20 @@
+// Internal dependencies
+import { hideToggle, showToggle } from 'actions/ui/toggle';
+
+// Module variables
+let store;
+
+/**
+ * Provides the global store to `sections`. This is optional and allows
+ * sections to toggle a loading state when a section is loading.
+ *
+ * @param {object} storeGlobal - Reference to the global store
+ *
+ */
+export const provideStore = storeGlobal => {
+	store = storeGlobal;
+};
+
 export const sections = {
 	myDomains: () => System.import( './my-domains' ),
 	hosts: () => System.import( './hosts' ),
@@ -5,5 +22,13 @@ export const sections = {
 };
 
 export const getComponent = ( sectionSlug, routeSlug ) => ( location, callback ) => {
-	sections[ sectionSlug ]().then( module => callback( null, module.default[ routeSlug ].default ) );
+	const dispatch = store && store.dispatch ? store.dispatch : () => {};
+
+	dispatch( showToggle( 'isSectionLoading' ) );
+
+	sections[ sectionSlug ]().then( module => {
+		dispatch( hideToggle( 'isSectionLoading' ) );
+
+		callback( null, module.default[ routeSlug ].default );
+	} );
 };

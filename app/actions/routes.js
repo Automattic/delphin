1b61498a0faecc2df6d20a4c392b74871779cabe
Yury Michurin <yury@nix.co.il>
2016-09-27 15:55:13 +0300

    Refactor redirect actionCreator to accept params

--- app/actions/routes.js
+++ app/actions/routes.js
@@ -4,7 +4,23 @@ import { push } from 'react-router-redux';
 // Internal dependencies
 import { getPath } from 'app/routes';
 
-export const redirect = ( pathSlug, query ) => push( {
-	pathname: getPath( pathSlug ),
-	query,
-} );
+/***
+ * Creates a redirect push action
+ * @param {string} pathSlug a path slug as appears in route definitions
+ * @param {{queryParams: string, pathParams:string}} options object that has queryParams - key value pairs of query
+ * parameters and pathParams a route parameters as defined in routes definitions
+ * @returns {Object} push redirect action
+ */
+export const redirect = ( pathSlug, options = {} ) => {
+	const { queryParams, pathParams } = options;
+
+	const locationDescriptor = {
+		pathname: getPath( pathSlug, pathParams )
+	};
+
+	if ( queryParams ) {
+		locationDescriptor.query = queryParams;
+	}
+
+	return push( locationDescriptor );
+};

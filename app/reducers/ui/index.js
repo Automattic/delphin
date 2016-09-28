7b05bce22ffaa5191e59612ec6d868e223ab77ba
Drew Blaisdell <drew.blaisdell@gmail.com>
2016-09-28 14:25:16 -0700

    Add `state.ui.isSectionLoading` reducer/selector/actions

--- app/reducers/ui/index.js
+++ app/reducers/ui/index.js
@@ -4,6 +4,7 @@ import { combineReducers } from 'redux';
 // Internal dependencies
 import { contactInformation } from './contact-information';
 import domainSearch from './domain-search';
+import isSectionLoading from './is-section-loading';
 import languagePicker from './language-picker';
 import myDomains from './my-domains';
 import toggle from './toggle';
@@ -11,6 +12,7 @@ import toggle from './toggle';
 export default combineReducers( {
 	contactInformation,
 	domainSearch,
+	isSectionLoading,
 	languagePicker,
 	myDomains,
 	toggle,

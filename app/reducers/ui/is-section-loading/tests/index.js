ad2029be7d74f27681f1a15cd589616139d548a4
Drew Blaisdell <drew.blaisdell@gmail.com>
2016-09-28 14:19:05 -0700

    Add tests for `state.ui.isSectionLoading` reducer

---
+++ app/reducers/ui/is-section-loading/tests/index.js
@@ -0,0 +1,37 @@
+jest.disableAutomock();
+
+// Internal dependencies
+import {
+	SECTION_FETCH,
+	SECTION_FETCH_COMPLETE,
+} from 'reducers/action-types';
+import isSectionLoading from '..';
+
+describe( 'state.ui.isSectionLoading', () => {
+	it( 'should return an empty object initially', () => {
+		expect( isSectionLoading( undefined, { type: 'UNRELATED_ACTION' } ) ).toEqual( {} );
+	} );
+
+	it( 'should set a key/value pair to true with `SECTION_FETCH`', () => {
+		expect( isSectionLoading( {}, { type: SECTION_FETCH, section: 'myDomains' } ) ).toEqual( {
+			myDomains: true
+		} );
+	} );
+
+	it( 'should handle multiple keys', () => {
+		expect( isSectionLoading( {
+			myDomains: true,
+			checkout: false,
+		}, { type: SECTION_FETCH, section: 'hosts' } ) ).toEqual( {
+			myDomains: true,
+			checkout: false,
+			hosts: true,
+		} );
+	} );
+
+	it( 'should set a key/value pair to false with `SECTION_FETCH_COMPLETE`', () => {
+		expect( isSectionLoading( { myDomains: true }, { type: SECTION_FETCH_COMPLETE, section: 'myDomains' } ) ).toEqual( {
+			myDomains: false
+		} );
+	} );
+} );

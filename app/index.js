5eae82ec961a06876161178f70da8998d354323e
Ben Dwyer <ben@scruffian.com>
2016-09-30 14:13:21 +0100

    Change the way we rerender the app when we switch locale, so that form fields aren't cleared

--- app/index.js
+++ app/index.js
@@ -4,18 +4,37 @@ import { Router } from 'react-router';
 
 // Internal dependencies
 import DocumentTitle from 'components/ui/document-title';
+import i18n from 'i18n-calypso';
 import { routes } from 'routes';
 
-const App = function( { history } ) {
-	return (
-		<DocumentTitle>
-			<Router history={ history } routes={ routes } />
-		</DocumentTitle>
-	);
-};
+const App = React.createClass( {
+	propTypes: {
+		history: PropTypes.object.isRequired
+	},
 
-App.propTypes = {
-	history: PropTypes.object.isRequired
-};
+	componentWillMount() {
+		i18n.stateObserver.on( 'change', this.reRender );
+	},
+
+	getInitialState() {
+		return {
+			key: 1
+		};
+	},
+
+	reRender() {
+		this.setState( {
+			key: Math.random()
+		} );
+	},
+
+	render() {
+		return (
+			<DocumentTitle key={ this.state.key }>
+				<Router history={ this.props.history } routes={ routes } />
+			</DocumentTitle>
+		);
+	}
+} );
 
 export default App;

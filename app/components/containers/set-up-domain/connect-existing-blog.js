a8cf42251c8f9b94e1a7bf257d511c1fad57f5c6
Stéphane Thomas <email@stephanethomas.com>
2016-10-10 17:05:56 +0200

    Domain Setup: Display host name instead of url on the Connect Existing Blog page

--- app/components/containers/set-up-domain/connect-existing-blog.js
+++ app/components/containers/set-up-domain/connect-existing-blog.js
@@ -1,14 +1,28 @@
 // External dependencies
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
+import get from 'lodash/get';
 
 // Internal dependencies
 import ConnectExistingBlog from 'components/ui/set-up-domain/connect-existing-blog';
+import { extractHostName } from 'lib/domains';
 import { redirect } from 'actions/routes';
+
+/**
+ * Retrieves the host name from the url entered at the previous step.
+ *
+ * @param {object} state - state tree
+ * @returns {string|null} - the host name or null if not found
+ */
+const getHostName = ( state ) => {
+	const url = get( state, 'form.findExistingBlog.url.value' );
+
+	return extractHostName( url );
+};
 
 export default connect(
 	( state, ownProps ) => ( {
-		blogUrl: 'www.blog.com',
+		hostName: getHostName( state ),
 		domainName: ownProps.params.domainName
 	} ),
 	dispatch => bindActionCreators( {

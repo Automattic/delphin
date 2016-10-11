d1775a553cf41c2ac80552a3658cf01b1a011d84
Stéphane Thomas <email@stephanethomas.com>
2016-10-11 13:26:09 +0200

    Domain Setup: Add selector to retrieve blog type

--- app/components/containers/set-up-domain/connecting-new-blog.js
+++ app/components/containers/set-up-domain/connecting-new-blog.js
@@ -4,11 +4,12 @@ import { connect } from 'react-redux';
 
 // Internal dependencies
 import ConnectingBlog from 'components/ui/set-up-domain/connecting-blog';
+import { getBlogType } from 'reducers/form/selectors';
 import { redirect } from 'actions/routes';
 
 export default connect(
 	( state, ownProps ) => ( {
-		blogType: 'new',
+		blogType: getBlogType( state ),
 		domainName: ownProps.params.domainName,
 	} ),
 	( dispatch, ownProps ) => bindActionCreators( {

ae46edfda26dce2db51e33bd0a77d45ea240fafe
Stéphane Thomas <email@stephanethomas.com>
2016-10-13 15:00:38 +0200

    Make all image paths relative
    This removes references to images deployed on our CDN.

--- app/components/ui/hosts/host-thumbnail.js
+++ app/components/ui/hosts/host-thumbnail.js
@@ -15,7 +15,7 @@ const HostThumbnail = ( { domainName, slug, name, shortDescription } ) => (
 		<p className={ styles.thumbnailDescription }>{ shortDescription }</p>
 		<Link className={ styles.thumbnailLearnMore } to={ getPath( 'hostInfo', { domainName, slug } ) }>{ i18n.translate( 'Learn More' ) }</Link>
 		<a className={ styles.thumbnailConnect }>
-			<img className={ styles.thumbnailLinkIcon } src="https://s0.wp.com/wp-content/themes/a8c/getdotblog/public/images/link.svg" />
+			<img className={ styles.thumbnailLinkIcon } src="/images/link.svg" />
 			{ i18n.translate( 'Connect Now' ) }
 		</a>
 	</li>
--- app/components/ui/search-input/related-words.js
+++ app/components/ui/search-input/related-words.js
@@ -76,7 +76,7 @@ class RelatedWords extends Component {
 
 							{ isGoogleTranslateAttributionVisible && (
 								<li className={ styles.googleTranslateAttribution }>
-									<img src="https://s0.wp.com/wp-content/themes/a8c/getdotblog/public/images/powered-by-google-translate.png" height={ 18 } width={ 175 } alt="Google Translate" />
+									<img src="/images/powered-by-google-translate.png" height={ 18 } width={ 175 } alt="Google Translate" />
 								</li>
 							) }
 						</ul>

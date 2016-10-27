31929419df6fae39dbd4ccc85e51bc89456e5eed
Grzegorz Ziolkowski <grzegorz@gziolo.pl>
2016-10-27 13:17:08 +0200

    Framework: Disable new Eslint rules that trigger errors

--- .eslintrc.js
+++ .eslintrc.js
@@ -16,15 +16,15 @@ module.exports = {
 		'react/prop-types': 2,
 		'react/sort-prop-types': 2,
 
-		// Legacy
-		'max-len': [ 1, { code: 140 } ],
-		'no-console': 1,
-		'prefer-const': 1,
-		'react/jsx-no-target-blank': 1,
-		'react/prefer-es6-class': 1,
-		'quote-props': [ 1, 'as-needed' ],
+		// Disable new rules that trigger errors
+		'max-len': 0,
+		'no-console': 0,
+		'prefer-const': 0,
+		'react/jsx-no-target-blank': 0,
+		'react/prefer-es6-class': 0,
+		'quote-props': 0,
 		'wpcalypso/import-docblock': 0,
-		'wpcalypso/jsx-classname-namespace': 1,
-		'wpcalypso/jsx-gridicon-size': 1
+		'wpcalypso/jsx-classname-namespace': 0,
+		'wpcalypso/jsx-gridicon-size': 0
 	}
 };

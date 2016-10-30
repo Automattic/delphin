23bb72249bf8eb62414a8a389c59a664485b53f8
Drew Blaisdell <drew.blaisdell@gmail.com>
2016-10-30 17:36:30 +0100

    Update `isRequesting` when updating nameservers in the `state.nameservers` reducer

--- app/reducers/nameservers/index.js
+++ app/reducers/nameservers/index.js
@@ -1,15 +1,18 @@
 // Internal dependencies
 import { createRequestReducer } from 'lib/create-request-reducer';
 import {
+	NAMESERVERS_UPDATE,
+	NAMESERVERS_UPDATE_COMPLETE,
+	NAMESERVERS_UPDATE_FAIL,
 	NAMESERVERS_FETCH,
 	NAMESERVERS_FETCH_COMPLETE,
 	NAMESERVERS_FETCH_FAIL,
 } from 'reducers/action-types';
 
 const reducer = createRequestReducer( {
-	loading: NAMESERVERS_FETCH,
-	success: NAMESERVERS_FETCH_COMPLETE,
-	fail: NAMESERVERS_FETCH_FAIL,
+	loading: [ NAMESERVERS_FETCH, NAMESERVERS_UPDATE ],
+	success: [ NAMESERVERS_FETCH_COMPLETE, NAMESERVERS_UPDATE_COMPLETE ],
+	fail: [ NAMESERVERS_FETCH_FAIL, NAMESERVERS_UPDATE_FAIL ],
 } );
 
 export default reducer;

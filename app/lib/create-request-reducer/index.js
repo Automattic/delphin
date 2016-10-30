0adbdeb029f6a252bd90d0ae9c42ac9e33fc4567
Drew Blaisdell <drew.blaisdell@gmail.com>
2016-10-30 17:23:34 +0100

    Update `createRequestReducer` to accept an array of action types

--- app/lib/create-request-reducer/index.js
+++ app/lib/create-request-reducer/index.js
@@ -8,14 +8,28 @@ export const initialState = {
 	error: null
 };
 
+/**
+ * Determines if the given action type is equal to, or in the array of, given types.
+ *
+ * @param {string} type - Action type
+ * @param {string|array} types - Action type or array of action types
+ *
+ * @return {bool} - If the given type is equivalent to the given type(s).
+ */
+const includesAction = ( type, types ) => {
+	const newTypes = Array.isArray( types ) ? types : [ types ];
+
+	return newTypes.indexOf( type ) > -1;
+};
+
 export const createRequestReducer = ( { loading, success, fail, reset } = {}, additionalReducer = x => x ) => ( state = initialState, action ) => {
 	const { type, error } = action;
 
-	if ( loading && type === loading ) {
+	if ( loading && includesAction( type, loading ) ) {
 		return Object.assign( {}, state, { isRequesting: true } );
 	}
 
-	if ( success && type === success ) {
+	if ( success && includesAction( type, success ) ) {
 		const data = action.data ? action.data : omit( action, 'type' );
 
 		return Object.assign( {}, state, {
@@ -25,7 +39,7 @@ export const createRequestReducer = ( { loading, success, fail, reset } = {}, ad
 		} );
 	}
 
-	if ( fail && type === fail ) {
+	if ( fail && includesAction( type, fail ) ) {
 		return Object.assign( {}, state, {
 			isRequesting: false,
 			error

53379074fbdaf09f8174c7f43d2c879c4e3708dd
Stéphane Thomas <email@stephanethomas.com>
2016-11-07 14:50:04 +0100

    My Domains: Add tracking of click event on Set Up button

--- app/components/ui/my-domains/domain-card/domain-not-connected.js
+++ app/components/ui/my-domains/domain-card/domain-not-connected.js
@@ -5,16 +5,21 @@ import React, { PropTypes } from 'react';
 import withStyles from 'isomorphic-style-loader/lib/withStyles';
 
 // Internal dependencies
-import Button from 'components/ui/button';
 import { getPath } from 'routes';
 import styles from './styles.scss';
+import TrackingButton from 'components/containers/tracking-button';
 
 const DomainNotConnected = ( { domainName } ) => {
 	return (
 		<div className={ classnames( styles.domainCard, styles.notConnected ) }>
 			<div className={ styles.domainHeading }>
 				<h3>{ domainName }</h3>
-				<Button href={ getPath( 'selectBlogType', { domainName } ) }>{ i18n.translate( 'Set up' ) }</Button>
+				<TrackingButton
+					href={ getPath( 'selectBlogType', { domainName } ) }
+					eventName="delphin_set_up_domain_click"
+				>
+					{ i18n.translate( 'Set up' ) }
+				</TrackingButton>
 			</div>
 			<div className={ styles.domainDetails }>
 				<p>{ i18n.translate( "You haven't set up this domain yet." ) }</p>

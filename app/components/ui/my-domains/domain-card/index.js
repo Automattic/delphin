1c994386eec69a6cff43a031e33c791607510b2d
Dan Hauk <hauk.dan@gmail.com>
2016-11-08 16:12:09 -0500

    Handle long domains, update copy, more CSS tweaks

--- app/components/ui/my-domains/domain-card/index.js
+++ app/components/ui/my-domains/domain-card/index.js
@@ -29,7 +29,7 @@ const DomainCard = ( { name, isSetup, detailsVisible } ) => {
 					<h3>{ name }</h3>
 				</div>
 				<div className={ styles.domainDetails }>
-					<p>{ i18n.translate( 'This domain was automatically set up to a WordPress.com site.' ) }</p>
+					<p>{ i18n.translate( 'This domain was automatically set up for your WordPress.com site.' ) }</p>
 				</div>
 				<div className={ styles.domainCardFooter }>
 					<a href="#" className={ styles.resetSettings }>{ i18n.translate( 'Reset to default settings' ) }</a>

ba22224b65150bd1759baff613463148a74ae0e0
Michael Shelton <mike.shelton@automattic.com>
2016-11-15 14:42:40 -0500

    Add setup label to titles

--- app/components/ui/set-up-domain/find-existing-blog/index.js
+++ app/components/ui/set-up-domain/find-existing-blog/index.js
@@ -65,7 +65,12 @@ class FindExistingBlog extends Component {
 
 				<div className={ styles.headerContainer }>
 					<div className={ styles.header }>
-						<h1 className={ styles.headerText }>{ i18n.translate( 'Connect to your blog' ) }</h1>
+						<h1 className={ styles.headerText }>
+							<span className={ styles.setUpLabel }>
+								{ i18n.translate( 'Setup: ' ) }
+							</span>
+							{ i18n.translate( 'Connect to your blog' ) }
+						</h1>
 						<ProgressBar progress={ 30 } />
 					</div>
 				</div>

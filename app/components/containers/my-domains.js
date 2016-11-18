07145289e09a16d337441f3a8f6178e04d404c1d
Grzegorz Ziolkowski <grzegorz@gziolo.pl>
2016-11-18 14:20:42 +0100

    My Domains: Remove no longer used code for domain card details toggle

--- app/components/containers/my-domains.js
+++ app/components/containers/my-domains.js
@@ -5,15 +5,12 @@ import { push } from 'react-router-redux';
 
 // Internal dependencies
 import { fetchMyDomains } from 'actions/my-domains';
-import { areDomainDetailsVisible } from 'reducers/ui/my-domains/selectors';
 import { getPath } from 'routes';
 import { isLoggedOut } from 'reducers/user/selectors';
 import MyDomains from 'components/ui/my-domains';
-import { showDomainDetails, hideDomainDetails } from 'actions/ui/my-domains';
 
 export default connect(
 	state => ( {
-		areDomainDetailsVisible: areDomainDetailsVisible( state ),
 		domains: state.user.myDomains,
 		isRequesting: state.domainSearch.isRequesting,
 		isLoggedOut: isLoggedOut( state )
@@ -21,18 +18,7 @@ export default connect(
 	dispatch => (
 		bindActionCreators( {
 			fetchMyDomains,
-			redirectToHome: () => push( getPath( 'home' ) ),
-			showDomainDetails,
-			hideDomainDetails
+			redirectToHome: () => push( getPath( 'home' ) )
 		}, dispatch )
-	),
-	( stateProps, dispatchProps, ownProps ) => Object.assign( {}, stateProps, dispatchProps, ownProps, {
-		toggleDomainDetails( domainName ) {
-			if ( stateProps.areDomainDetailsVisible( domainName ) ) {
-				dispatchProps.hideDomainDetails( domainName );
-			} else {
-				dispatchProps.showDomainDetails( domainName );
-			}
-		}
-	} )
+	)
 )( MyDomains );
--- app/components/ui/my-domains/domain-card-list/index.js
+++ app/components/ui/my-domains/domain-card-list/index.js
@@ -7,19 +7,14 @@ import styles from './styles.scss';
 import DomainCard from 'components/ui/my-domains/domain-card';
 import NewDomainCard from 'components/ui/my-domains/new-domain-card';
 
-const DomainCardList = ( { domains, areDetailsVisible, toggleDomainDetails } ) => {
-	const toggleDetails = ( domainName ) => () => toggleDomainDetails( domainName );
-
+const DomainCardList = ( { domains } ) => {
 	return (
 		<div className={ styles.domainCardList }>
-			{ domains.map( ( { hostName, id, name, service } ) =>
+			{ domains.map( ( { id, name, service } ) =>
 				<DomainCard
 					key={ id }
 					name={ name }
-					detailsVisible={ areDetailsVisible( name ) }
-					hostName={ hostName }
-					service={ service }
-					toggleDetails={ toggleDetails( name ) } />
+					service={ service } />
 			) }
 			<NewDomainCard />
 		</div>
@@ -27,9 +22,7 @@ const DomainCardList = ( { domains, areDetailsVisible, toggleDomainDetails } ) =
 };
 
 DomainCardList.propTypes = {
-	areDetailsVisible: PropTypes.func.isRequired,
-	domains: PropTypes.array.isRequired,
-	toggleDomainDetails: PropTypes.func.isRequired
+	domains: PropTypes.array.isRequired
 };
 
 export default withStyles( styles )( DomainCardList );

1eefca05a82d07bbe1f2743509925062ffbcc097
Michael Shelton <mike.shelton@automattic.com>
2016-11-23 13:06:53 -0500

    Add new illustrations and remove nameserver loading state

--- app/components/ui/my-domains/domain-card/domain-with-custom-nameservers.js
+++ app/components/ui/my-domains/domain-card/domain-with-custom-nameservers.js
@@ -23,7 +23,6 @@ class DomainWithCustomNameservers extends Component {
 		const {
 			domainName
 		} = this.props;
-		const nameservers = [];
 
 		return (
 			<div className={ classNames( styles.domainCard, styles.connectedNameservers ) }>
@@ -32,21 +31,11 @@ class DomainWithCustomNameservers extends Component {
 				</div>
 				<div className={ styles.domainDetails }>
 					<p>{ i18n.translate( 'This domain has custom name servers.' ) }</p>
-					<div className={ styles.nameservers }>
-						<div className={ styles.nameserversLoading }>
-							{ i18n.translate( 'Fetching name servers…' ) }
-						</div>
-						<ul className={ styles.nameserversList }>
-							{ nameservers.map( nameserver => (
-								<li>{ nameserver }</li>
-							) ) }
-						</ul>
-					</div>
 				</div>
 				<div className={ styles.domainCardFooter }>
 					<Link
 						to={ getPath( 'updateNameservers', { domainName } ) }
-					>{ i18n.translate( 'Change name servers' ) }</Link>
+					>{ i18n.translate( 'Edit name servers' ) }</Link>
 					<a
 						href="#"
 						className={ styles.resetSettings }

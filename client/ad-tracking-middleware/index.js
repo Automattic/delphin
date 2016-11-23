eb0ec1f772c2ceeacf2db771f2453a01fe94f87d
Tugdual de Kerviler <dekervit@gmail.com>
2016-11-23 12:25:34 +0900

    Add retargetting for bing, quantcast, facebook and refactor googleads

--- client/ad-tracking-middleware/index.js
+++ client/ad-tracking-middleware/index.js
@@ -1,25 +1,16 @@
 // Internal dependencies
-import config, { isEnabled } from 'config';
-import { loadScript } from 'lib/load-script';
+import analytics from 'lib/analytics';
 import { TRANSACTION_CREATE_COMPLETE } from 'reducers/action-types';
-
-if ( isEnabled( 'ad_tracking' ) ) {
-	loadScript( 'https://www.googleadservices.com/pagead/conversion_async.js' );
-}
 
 export const adTrackingMiddleware = () => next => action => {
 	const { type } = action;
 
 	switch ( type ) {
 		case TRANSACTION_CREATE_COMPLETE:
-			// users might block google_trackConversion, we must not fail because of it
-			if ( isEnabled( 'ad_tracking' ) && typeof window.google_trackConversion === 'function' ) {
-				window.google_trackConversion( {
-					google_conversion_id: config( 'google_conversion_id' ),
-					google_conversion_format: 3,
-					google_conversion_label: config( 'google_conversion_label' ),
-				} );
-			}
+			const domainCostDetail = action.domain.details.find( detail => detail.productSlug === 'delphin-domain' ),
+				domainCost = domainCostDetail ? domainCostDetail.cost.match( /\d+/ )[ 0 ] : null,
+				domainCurrencyCode = action.domain.currencyCode;
+			analytics.conversion.recordPurchase( action.receiptId, domainCost, domainCurrencyCode );
 	}
 
 	return next( action );

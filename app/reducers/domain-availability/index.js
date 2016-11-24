d37352e7ba092a402f9af112ec0cf233096caffe
Ben Dwyer <ben@scruffian.com>
2016-11-24 16:06:37 +0000

    Adding a request reducer

---
+++ app/reducers/domain-availability/index.js
@@ -0,0 +1,28 @@
+// Internal dependencies
+import { createRequestReducer } from 'lib/create-request-reducer';
+import {
+	DOMAIN_AVAILABILITY_FETCH,
+	DOMAIN_AVAILABILITY_FETCH_COMPLETE,
+	DOMAIN_AVAILABILITY_FETCH_FAIL,
+} from 'reducers/action-types';
+
+export const domainAvailability = ( state = {}, action ) => {
+	const { domainName } = action;
+
+	if ( domainName && state.domainName ) {
+		return state;
+	}
+
+	if ( ! domainName ) {
+		return state;
+	}
+
+	const domainState = {};
+	domainState[ domainName ] = createRequestReducer( {
+		loading: DOMAIN_AVAILABILITY_FETCH,
+		success: DOMAIN_AVAILABILITY_FETCH_COMPLETE,
+		fail: DOMAIN_AVAILABILITY_FETCH_FAIL
+	} )( state[ domainName ], action );
+
+	return Object.assign( {}, state, domainState );
+};
---
+++ app/reducers/domain-availability/tests/index.js
@@ -0,0 +1,108 @@
+// Internal dependencies
+import { domainAvailability } from '..';
+
+describe( 'domain availability reducer', () => {
+	it( 'should throw an error when state and action are undefined', () => {
+		expect( domainAvailability ).toThrowError( TypeError );
+	} );
+
+	it( 'should throw an error when action is undefined', () => {
+		expect( () => {
+			return domainAvailability( {} );
+		} ).toThrowError( TypeError );
+	} );
+
+	it( 'should return initial state when state is undefined and action is empty', () => {
+		expect( domainAvailability( undefined, {} ) ).toEqual( {} );
+	} );
+
+	it( 'should return original state when action is empty', () => {
+		const originalState = Object.freeze( {
+				'domain-name.blog': {
+					data: null,
+					error: null,
+					isRequesting: true,
+					hasLoadedFromServer: false,
+				}
+			} ),
+			newState = domainAvailability( originalState, {} );
+
+		expect( newState ).toEqual( originalState );
+	} );
+
+	it( 'should return original state when action type is not supported', () => {
+		const originalState = Object.freeze( {
+				'domain-name.blog': {
+					data: null,
+					error: null,
+					isRequesting: true,
+					hasLoadedFromServer: false,
+				}
+			} ),
+			newState = domainAvailability( originalState, { type: 'ORDER_CHEESE_BURGER' } );
+
+		expect( newState ).toEqual( originalState );
+	} );
+
+	it( 'should return original state when a domainName is not specified', () => {
+		const originalState = Object.freeze( {
+				'domain-name.blog': {
+					data: null,
+					error: null,
+					isRequesting: true,
+					hasLoadedFromServer: false,
+				}
+			} ),
+			newState = domainAvailability( originalState, { type: 'DOMAIN_AVAILABILITY_FETCH' } );
+
+		expect( newState ).toEqual( originalState );
+	} );
+
+	it( 'should add a new domain to the state when there is a domainName in the action', () => {
+		const originalState = Object.freeze( {
+				'domain-name.blog': {
+					data: null,
+					error: null,
+					isRequesting: true,
+					hasLoadedFromServer: false,
+				}
+			} ),
+			newState = domainAvailability( originalState, { type: 'DOMAIN_AVAILABILITY_FETCH', domainName: 'another-domain-name.blog' } ),
+			expectedState = Object.freeze( {
+				'domain-name.blog': {
+					data: null,
+					error: null,
+					isRequesting: true,
+					hasLoadedFromServer: false,
+				},
+				'another-domain-name.blog': {
+					data: null,
+					error: null,
+					isRequesting: true,
+					hasLoadedFromServer: false,
+				}
+			} );
+
+		expect( newState ).toEqual( expectedState );
+	} );
+
+	it( 'should NOT overwrite the domain in the state when it already exists', () => {
+		const originalState = Object.freeze( {
+				'domain-name.blog': {
+					data: null,
+					error: null,
+					isRequesting: true,
+					hasLoadedFromServer: false,
+				},
+				'another-domain-name.blog': {
+					data: { 'some': 'dummy-data' },
+					error: null,
+					isRequesting: true,
+					hasLoadedFromServer: false,
+				}
+			} ),
+			newState = domainAvailability( originalState, { type: 'DOMAIN_AVAILABILITY_FETCH', domainName: 'another-domain-name.blog' } );
+
+		expect( newState ).toEqual( originalState );
+	} );
+} );

1f341547cce9ca8aeaf6a1c264523fcb2956a9c4
Drew Blaisdell <drew.blaisdell@gmail.com>
2016-11-24 16:12:27 +0900

    Fix error when logging out on `Success`

--- app/components/containers/success.js
+++ app/components/containers/success.js
@@ -6,14 +6,16 @@ import { bindActionCreators } from 'redux';
 import { destroySetupForms } from 'actions/setup';
 import { fetchMyDomains } from 'actions/my-domains';
 import { getSelectedDomain, hasSelectedDomain } from 'reducers/checkout/selectors';
-import { getUserSettings } from 'reducers/user/selectors';
+import { getUserSettings, isLoggedIn } from 'reducers/user/selectors';
 import Success from 'components/ui/success';
 import { redirect } from 'actions/routes';
 
 export default connect(
 	state => ( {
 		domain: getSelectedDomain( state ).domainName,
-		email: hasSelectedDomain( state ) ? getUserSettings( state ).data.email : null,
+		email: ( hasSelectedDomain( state ) && isLoggedIn( state ) )
+			? getUserSettings( state ).data.email
+			: null,
 		hasSelectedDomain: hasSelectedDomain( state ),
 	} ),
 	dispatch => bindActionCreators( {

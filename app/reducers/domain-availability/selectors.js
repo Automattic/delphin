0ce3c969b1a80e335bd427b2258f4a311d8e3898
Ben Dwyer <ben@scruffian.com>
2016-11-25 12:42:42 +0000

    Add a selector to get the loading state of the availability check

--- app/reducers/domain-availability/selectors.js
+++ app/reducers/domain-availability/selectors.js
@@ -1,8 +1,23 @@
 /**
+ * Gets the status of the availability of a given domain name from the state
+ *
+ * @param {object} state - Global store
+ * @param {string} domainName - The domain name to find out about
+ * @return {bool} Whether or not the domain availability has been fetched
+ */
+export const getDomainAvailabilityLoading = ( state, domainName ) => {
+	if ( ! state.domainAvailability[ domainName ] ) {
+		return false;
+	}
+
+	return state.domainAvailability[ domainName ].hasLoadedFromServer;
+};
+
+/**
  * Gets the availability of a given domain name from the state
  *
  * @param {object} state - Global store
- * @param {domainName} string - The domain name to find out about
+ * @param {string} domainName - The domain name to find out about
  * @return {bool} Whether or not the domain is available
  */
 export const getDomainAvailability = ( state, domainName ) => {

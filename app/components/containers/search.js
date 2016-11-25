93cfeb0154af74cc6d149e6991b247bde6d8a974
Drew Blaisdell <drew.blaisdell@gmail.com>
2016-11-25 12:34:55 +0900

    Remove unnecessary `selectDomain` function in `mergeDispatchWithProps`

--- app/components/containers/search.js
+++ app/components/containers/search.js
@@ -86,10 +86,6 @@ export default connect(
 			}
 
 			dispatchProps.fetchDomainSuggestions( query );
-		},
-
-		selectDomain( domainProduct ) {
-			dispatchProps.selectDomain( domainProduct );
 		}
 	} )
 )( Search );

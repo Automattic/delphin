f0b329ec2cd06da828ce96826d0fe92e5271b5ec
Drew Blaisdell <drew.blaisdell@gmail.com>
2016-11-29 15:26:11 +0900

    Disable `Suggestion` when other suggestion is fetching availability

--- app/reducers/domain-availability/selectors.js
+++ app/reducers/domain-availability/selectors.js
@@ -14,6 +14,20 @@ export const isDomainAvailabilityRequesting = ( state, domainName ) => {
 };
 
 /**
+ * Determines whether another domain is having its availability checked.
+ *
+ * @param {object} state - Global store
+ * @param {string} domainName - The domain name to find out about
+ * @return {bool} Whether or not the availability of a domain that is not the
+ * given domain is being fetched.
+ */
+export const isDomainAvailabilityRequestingOtherDomain = ( state, domainName ) => (
+	Object.keys( state.domainAvailability ).some( name => (
+		isDomainAvailabilityRequesting( state, name ) && name !== domainName
+	) )
+);
+
+/**
  * Gets the loaded status of the availability of a given domain name from the state
  *
  * @param {object} state - Global store

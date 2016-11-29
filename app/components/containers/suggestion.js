b9ed7db8e7fe30144e2258af84111a1332bb2097
Grzegorz Ziolkowski <grzegorz@gziolo.pl>
2016-11-29 12:36:27 +0100

    Search: Refactor connect method for Suggestion container

--- app/components/containers/suggestion.js
+++ app/components/containers/suggestion.js
@@ -9,7 +9,7 @@ import {
 	isDomainAvailabilityRequesting,
 	isDomainAvailabilityRequestingOtherDomain,
 } from 'reducers/domain-availability/selectors';
-import { recordTracksEvent } from 'actions/analytics';
+import { recordTracksEvent, withAnalytics } from 'actions/analytics';
 import Suggestion from 'components/ui/search/suggestion';
 
 export default connect(
@@ -19,14 +19,13 @@ export default connect(
 		isRequestingAvailabilityForOtherDomain: isDomainAvailabilityRequestingOtherDomain( state, ownProps.suggestion.domainName ),
 		hasLoadedAvailability: hasDomainAvailabilityLoaded( state, ownProps.suggestion.domainName )
 	} ),
-	( dispatch ) => ( {
-		checkDomainAvailability( domainProduct ) {
-			dispatch( recordTracksEvent( 'delphin_domain_availability_check', {
-				is_premium: domainProduct.isPremium,
-				relevance: domainProduct.relevance
-			} ) );
-
-			dispatch( checkDomainAvailability( domainProduct ) );
-		}
-	} )
+	{
+		checkDomainAvailability: withAnalytics(
+			( { isPremium, relevance } ) => recordTracksEvent( 'delphin_domain_availability_check', {
+				is_premium: isPremium,
+				relevance
+			} ),
+			checkDomainAvailability
+		)
+	}
 )( Suggestion );

761a0cc215276dad407dce1d2f08decab96e2ca4
Grzegorz Ziolkowski <grzegorz@gziolo.pl>
2016-12-01 15:22:22 +0100

    Checkout: Add confirm trademark page skeleton

---
+++ app/components/ui/confirm-trademark/index.js
@@ -0,0 +1,12 @@
+// External dependencies
+import React from 'react';
+import withStyles from 'isomorphic-style-loader/lib/withStyles';
+
+// Internal dependencies
+import styles from './styles.scss';
+
+const ConfirmTrademark = () => (
+	<div>Hello world</div>
+);
+
+export default withStyles( styles )( ConfirmTrademark );

24e0dddb3bfa6399a431e2a4d922913e83f6ab76
Grzegorz Ziolkowski <grzegorz@gziolo.pl>
2016-12-05 16:29:21 +0100

    Trademark claim: Update texts and simplify display logic

--- app/components/ui/confirm-trademark/index.js
+++ app/components/ui/confirm-trademark/index.js
@@ -8,18 +8,26 @@ import { bindHandlers } from 'react-bind-handlers';
 import styles from './styles.scss';
 import { getPath } from 'routes';
 import Button from 'components/ui/button';
-import PartialUnderline from 'components/ui/partial-underline';
 import DocumentTitle from 'components/ui/document-title';
 import SunriseStep from 'components/ui/sunrise-step';
 import TrackingLink from 'components/containers/tracking-link';
 
 class ConfirmTrademark extends React.Component {
+	isDataLoaded( props = this.props ) {
+		const {
+			domainName,
+			hasLoadedAvailability,
+			hasSelectedDomain,
+			selectedDomain
+		} = props;
+
+		return hasLoadedAvailability && hasSelectedDomain && selectedDomain.domainName === domainName;
+	}
+
 	componentDidMount() {
 		const {
-			hasLoadedAvailability,
 			hasTrademarkClaim,
-			hasSelectedDomain,
-			selectedDomain,
+			isAvailable,
 			domainName,
 			checkDomainAvailability,
 			fetchDomainPrice,
@@ -27,95 +35,85 @@ class ConfirmTrademark extends React.Component {
 			selectDomain
 		} = this.props;
 
-		if ( ! hasLoadedAvailability || ! hasSelectedDomain || selectedDomain.domainName !== domainName ) {
+		if ( ! this.isDataLoaded() ) {
 			checkDomainAvailability( { domainName } );
 			const pricePromise = fetchDomainPrice( domainName );
 
 			pricePromise.then( priceResult => selectDomain( priceResult.result ) );
-		} else if ( ! hasTrademarkClaim &&
-				hasSelectedDomain &&
-				selectedDomain.domainName === domainName ) {
+		} else if ( ! isAvailable ) {
+			redirect( 'home' );
+		} else if ( ! hasTrademarkClaim ) {
 			redirect( 'contactInformation' );
+		}
+	}
+
+	componentWillReceiveProps( nextProps ) {
+		const {
+			isAvailable,
+			hasTrademarkClaim,
+			redirect
+		} = nextProps;
+
+		if ( this.isDataLoaded( nextProps ) ) {
+			if ( ! isAvailable ) {
+				redirect( 'home' );
+			} else if ( ! hasTrademarkClaim ) {
+				redirect( 'contactInformation' );
+			}
 		}
 	}
 
 	handleSubmit( event ) {
 		event.preventDefault();
 
-		const { redirect } = this.props;
-
-		redirect( 'contactInformation' );
-	}
-
-	componentWillReceiveProps( nextProps ) {
-		const {
-			hasLoadedAvailability,
-			hasSelectedDomain,
-			isAvailable,
-			hasTrademarkClaim,
-			selectedDomain,
-			domainName,
-			redirect
-		} = nextProps;
-
-		if ( hasLoadedAvailability && isAvailable && hasSelectedDomain && selectedDomain.domainName === domainName && ! hasTrademarkClaim ) {
-			redirect( 'contactInformation' );
-		}
+		this.props.redirect( 'contactInformation' );
 	}
 
 	render() {
-		const {
-			domainName,
-			hasLoadedAvailability,
-			isAvailable,
-			hasSelectedDomain,
-			hasTrademarkClaim,
-			selectedDomain
-		} = this.props;
-		const requestedDomainHasClaim = ( hasLoadedAvailability &&
-			hasTrademarkClaim &&
-			hasSelectedDomain &&
-			selectedDomain.domainName === domainName
-		);
+		const domainName = this.props.domainName;
+
+		if ( ! this.isDataLoaded() ) {
+			return (
+				<SunriseStep>
+					<p>{ i18n.translate( 'Loading…' ) }</p>
+				</SunriseStep>
+			);
+		}
 
 		return ( <SunriseStep>
-			<DocumentTitle title={ i18n.translate( 'Confirm your trademark' ) } />
-			{
-				hasLoadedAvailability
-					? <SunriseStep.Header>
-						<h1>
-							{ i18n.translate( 'Confirm your trademark' ) }
-						</h1>
-						<h2>
-							{ isAvailable
-								? i18n.translate( 'This domain is available, however there is a trademark claim on it' )
-								: i18n.translate( 'This domain is not available' )
-							}
-						</h2>
-					</SunriseStep.Header>
-					: <SunriseStep.Header><h1>{ i18n.translate( 'Loading…' ) }</h1></SunriseStep.Header>
-			}
-			<SunriseStep.Form onSubmit={ this.handleSubmit }>
-				<PartialUnderline>
-					<h3>{ domainName }</h3>
-				</PartialUnderline>
+			<DocumentTitle title={ i18n.translate( 'Trademark claim' ) } />
 
-				{ hasLoadedAvailability &&
+			<SunriseStep.Header>
+				<h1>
+					{ i18n.translate( 'Trademark claim' ) }
+				</h1>
+			</SunriseStep.Header>
+
+			<SunriseStep.Form onSubmit={ this.handleSubmit }>
+				<div className={ styles.trademarkWarning }>
+					<p>{ i18n.translate( '%(domainName)s matches a known trademark.', {
+						args: { domainName }
+					} ) }</p>
+				</div>
+
 				<div className={ styles.trademarkNotice }>
 					<p>
-						{ i18n.translate( 'If you continue you will be sent an email to review the trademark notice before we ' +
-							'can complete your registration.' ) }
+						{ i18n.translate( 'You can register this domain, but you will have to confirm that your registration will not infringe on the trademark rights. The trademark holder will be notified, and they may choose to dispute your registration.' ) }
 					</p>
 					<p>
-						{ i18n.translate( 'Your order will be cancelled if you do not respond to this request within 48 hours.' ) }
+						{ i18n.translate( "We'll send you an email with further instructions after you submit your order. You will have to review the terms and agree to them within 48 hours, or your registration will be canceled." ) }
 					</p>
-				</div> }
+				</div>
 
-				<Button disabled={ ( ! requestedDomainHasClaim || ! isAvailable ) }>
+				<Button>
 					{ i18n.translate( 'Continue with trademark registration' ) }
 				</Button>
-
 			</SunriseStep.Form>
+
+			<SunriseStep.Footer>
+				<p>{ i18n.translate( "You'll get an automatic refund if you reject the terms or if you don't agree to them on time." ) }</p>
+			</SunriseStep.Footer>
+
 			<div className={ styles.backNotice }>
 				<TrackingLink eventName="delphin_start_over_click" to={ getPath( 'home' ) }>
 					{ i18n.translate( 'Find a different domain' ) }

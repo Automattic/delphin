1b93c741e930e37310aee4f87c74902504a33388
Tugdual de Kerviler <dekervit@gmail.com>
2016-12-08 17:04:18 +0800

    Update Success page to display a different message for trademarked domains

--- app/components/containers/success.js
+++ app/components/containers/success.js
@@ -3,18 +3,33 @@ import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 
 // Internal dependencies
+import { checkDomainAvailability } from 'actions/domain-availability';
 import { destroySetupForms } from 'actions/setup';
 import { fetchMyDomains } from 'actions/my-domains';
 import { getSelectedDomain } from 'reducers/checkout/selectors';
+import { hasDomainTrademarkClaim } from 'reducers/domain-availability/selectors';
+import { hasDomainAvailabilityLoaded } from 'reducers/domain-availability/selectors';
 import RequireLogin from 'components/containers/require-login';
 import Success from 'components/ui/success';
 
 export default connect(
-	state => ( {
-		domain: getSelectedDomain( state ).domainName
-	} ),
+	state => {
+		const domainName = getSelectedDomain( state ).domainName;
+
+		return {
+			domain: domainName,
+			hasTrademarkClaim: hasDomainTrademarkClaim( state, domainName ),
+			hasDomainAvailabilityLoaded: hasDomainAvailabilityLoaded( state, domainName ),
+		};
+	},
 	dispatch => bindActionCreators( {
+		checkDomainAvailability,
 		destroySetupForms,
 		fetchMyDomains
-	}, dispatch )
+	}, dispatch ),
+	( stateProps, dispatchProps, ownProps ) => Object.assign( {}, stateProps, dispatchProps, ownProps, {
+		checkDomainAvailability() {
+			dispatchProps.checkDomainAvailability( { domainName: stateProps.domain } );
+		}
+	} )
 )( RequireLogin( Success ) );

afb7388ce3588c902bb93210d0a2342df51ede19
Tugdual de Kerviler <dekervit@gmail.com>
2016-12-08 17:04:18 +0800

    Update Success page to display a different message for trademarked domains

--- app/components/ui/success/index.js
+++ app/components/ui/success/index.js
@@ -15,17 +15,65 @@ import { getPath } from 'routes';
 
 class Success extends React.Component {
 	componentDidMount() {
+		const { checkDomainAvailability, hasDomainAvailabilityLoaded } = this.props;
+
+		if ( ! hasDomainAvailabilityLoaded ) {
+			checkDomainAvailability();
+		}
+
 		this.props.fetchMyDomains();
 		this.props.destroySetupForms();
 	}
 
-	render() {
+	renderTrademarkedDomain() {
+		const { domain } = this.props;
+		return (
+			<div>
+				<SunriseStep.Header className={ styles.header }>
+					<h1 className={ styles.heading }>
+						{ i18n.translate( '{{strong}}%(domain)s{{/strong}} is almost yours!', {
+							args: { domain },
+							components: {
+								strong: <strong />
+							}
+						} ) }
+					</h1>
+				</SunriseStep.Header>
+
+				<div className={ styles.content }>
+
+					<div className={ styles.text }>
+						<p>
+							{ i18n.translate(
+								'You will get an email soon to review the Trademark Notice on this domain. ' +
+								'You must verify with Trademark Clearinghouse that your registration of this domain ' +
+								'will not infringe on the trademark.'
+							) }
+						</p>
+
+						<p>
+							{ i18n.translate(
+								'If you do not review and respond to this request within 48 hours ' +
+								'your registration will be cancelled.'
+							) }
+						</p>
+					</div>
+				</div>
+
+				<SunriseStep.Footer>
+					<Link to={ getPath( 'myDomains' ) }>
+						{ i18n.translate( 'Go to My Domains' ) }
+					</Link>
+				</SunriseStep.Footer>
+			</div>
+		);
+	}
+
+	renderDefaultDomain() {
 		const { domain } = this.props;
 
 		return (
-			<SunriseStep className={ styles.step }>
-				<DocumentTitle title={ i18n.translate( 'Success' ) } />
-
+			<div>
 				<SunriseStep.Header className={ styles.header }>
 					<h1 className={ styles.heading }>
 						{ i18n.translate( '{{strong}}%(domain)s{{/strong}} is now yours!', {
@@ -63,15 +111,44 @@ class Success extends React.Component {
 						{ i18n.translate( "I'll set up my domain later." ) }
 					</Link>
 				</SunriseStep.Footer>
+			</div>
+		);
+	}
+
+	render() {
+		const { hasDomainAvailabilityLoaded, hasTrademarkClaim } = this.props;
+
+		if ( ! hasDomainAvailabilityLoaded ) {
+			return (
+				<SunriseStep className={ styles.step }>
+					<DocumentTitle title={ i18n.translate( 'Success' ) } />
+
+					<SunriseStep.Header className={ styles.header }>
+						<h1 className={ styles.heading }>
+							{ i18n.translate( 'Loading…' ) }
+						</h1>
+					</SunriseStep.Header>
+				</SunriseStep>
+			);
+		}
+
+		return (
+			<SunriseStep className={ styles.step }>
+				<DocumentTitle title={ i18n.translate( 'Success' ) } />
+
+				{ hasTrademarkClaim ? this.renderTrademarkedDomain() : this.renderDefaultDomain() }
 			</SunriseStep>
 		);
 	}
 }
 
 Success.propTypes = {
+	checkDomainAvailability: PropTypes.func.isRequired,
 	destroySetupForms: PropTypes.func.isRequired,
 	domain: PropTypes.string,
-	fetchMyDomains: PropTypes.func.isRequired
+	fetchMyDomains: PropTypes.func.isRequired,
+	hasDomainAvailabilityLoaded: PropTypes.bool.isRequired,
+	hasTrademarkClaim: PropTypes.bool.isRequired
 };
 
 export default scrollToTop( withStyles( styles )( withPageView( Success, 'Success' ) ) );

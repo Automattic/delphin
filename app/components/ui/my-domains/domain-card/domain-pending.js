f00d81818ba6dd141a614007c6b03151547e4a7b
Tugdual de Kerviler <dekervit@gmail.com>
2016-12-16 17:02:02 +0900

    Add TM tag

--- app/components/ui/my-domains/domain-card/domain-pending.js
+++ app/components/ui/my-domains/domain-card/domain-pending.js
@@ -5,13 +5,23 @@ import React, { PropTypes } from 'react';
 import withStyles from 'isomorphic-style-loader/lib/withStyles';
 
 // Internal dependencies
+import Tag from 'components/ui/tag';
 import styles from './styles.scss';
 
 const DomainPending = ( { domainName } ) => {
 	return (
-		<div className={ classnames( styles.domainCard, styles.notConnected ) }>
+		<div className={ classnames( styles.domainCard ) }>
 			<div className={ styles.domainHeading }>
-				<h3>{ domainName }</h3>
+				<h3>
+					{ domainName }
+
+					<Tag
+						className={ styles.trademarkTag }
+						title={ i18n.translate( 'This domain has a trademark claim' ) }
+					>
+						TM
+					</Tag>
+				</h3>
 			</div>
 			<div className={ styles.domainDetails }>
 				<p>{ i18n.translate( 'Your action is needed. We sent you an email with instructions for finalizing this order.' ) }</p>

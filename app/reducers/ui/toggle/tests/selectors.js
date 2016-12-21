ef1632f0422336c1f5b6f2b35910b153d322c112
Drew Blaisdell <drew.blaisdell@gmail.com>
2016-12-21 11:16:13 -0800

    Update `toggle` reducer to allow for persistent toggles

--- app/reducers/ui/toggle/tests/selectors.js
+++ app/reducers/ui/toggle/tests/selectors.js
@@ -3,8 +3,8 @@ import { getToggle } from '../selectors';
 
 describe( 'getToggle', () => {
 	it( 'should return the value of `state.ui.toggle[name]` if it exists', () => {
-		expect( getToggle( { ui: { toggle: { foo: true } } }, 'foo' ) ).toEqual( true );
-		expect( getToggle( { ui: { toggle: { foo: false } } }, 'foo' ) ).toEqual( false );
+		expect( getToggle( { ui: { toggle: { foo: { value: true, persistent: false } } } }, 'foo' ) ).toEqual( true );
+		expect( getToggle( { ui: { toggle: { foo: { value: false, persistent: false } } } }, 'foo' ) ).toEqual( false );
 	} );
 
 	it( 'should return false if `state.ui.toggle[name]` does not exist', () => {

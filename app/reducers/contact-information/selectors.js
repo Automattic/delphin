0d82e59257d0108677226963050431e25861d21e
Stéphane Thomas <email@stephanethomas.com>
2016-12-27 19:00:59 +0100

    Checkout: Move selector out of container component
    This should live next to the reducer for contact information.

--- app/reducers/contact-information/selectors.js
+++ app/reducers/contact-information/selectors.js
@@ -2,7 +2,7 @@
 import get from 'lodash/get';
 
 /**
- * Retrieves the country code from the domain contact information form.
+ * Retrieves the country code from the contact information entered.
  *
  * @param {object} state - global state tree
  * @returns {string|null} - the country code, or null if not found
@@ -12,7 +12,23 @@ export const getCountryCode = ( state ) => {
 };
 
 /**
- * Retrieves the postal code from the domain contact information form.
+ * Retrieves the full name of the user from the contact information entered.
+ *
+ * @param {object} state - global state tree
+ * @returns {string} - the full name, or an empty string
+ */
+export const getFullName = ( state ) => {
+	if ( ! state.form.contactInformation ) {
+		return '';
+	}
+
+	const { firstName: { value: firstName }, lastName: { value: lastName } } = state.form.contactInformation;
+
+	return `${ firstName } ${ lastName }`;
+};
+
+/**
+ * Retrieves the postal code from the contact information entered.
  *
  * @param {object} state - global state tree
  * @returns {string|null} - the postal code, or null if not found

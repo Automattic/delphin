d08084d50affcfddd7e09fbc9ac818c89777cc94
Yury Michurin <yury@nix.co.il>
2016-12-29 12:56:20 +0200

    Factor our contact form to it's own component

--- app/components/ui/contact-information-form/index.js
+++ app/components/ui/contact-information-form/index.js
@@ -1,3 +1,368 @@
-/**
- * Created by yury on 28/12/2016.
- */
+// External dependencies
+import i18n from 'i18n-calypso';
+import React, { PropTypes } from 'react';
+import withStyles from 'isomorphic-style-loader/lib/withStyles';
+import { bindHandlers } from 'react-bind-handlers';
+
+// Internal dependencies
+import Button from 'components/ui/button';
+import Country from 'components/containers/country';
+import Form from 'components/ui/form';
+import Input from 'components/ui/form/input';
+import { isCallingCode } from 'lib/form';
+import Phone from 'components/ui/form/phone';
+import State from 'components/ui/form/state';
+import styles from './styles.scss';
+import ValidationError from 'components/ui/form/validation-error';
+
+class ContactInformationForm extends React.Component {
+	componentWillMount() {
+		if ( ! this.props.userLocation.isRequesting && ! this.props.userLocation.hasLoadedFromServer ) {
+			this.props.fetchLocation();
+		}
+
+		if ( ! this.props.contactInformation.isRequesting && ! this.props.contactInformation.hasLoadedFromServer ) {
+			this.props.fetchContactInformation();
+		}
+
+		if ( this.props.contactInformation.hasLoadedFromServer ) {
+			this.initializeContactInformation();
+		}
+
+		if ( this.canUpdateCountryFromLocation() ) {
+			this.setCountryCode();
+		}
+
+		this.props.resetInputVisibility();
+	}
+
+	componentWillReceiveProps( nextProps ) {
+		if ( this.isDataLoading() && ! this.isDataLoading( nextProps ) ) {
+			this.initializeContactInformation( nextProps );
+		}
+
+		if ( ! this.canUpdateCountryFromLocation() && this.canUpdateCountryFromLocation( nextProps ) ) {
+			this.setCountryCode( nextProps );
+		}
+
+		if ( this.shouldFetchStates( nextProps ) ) {
+			this.props.fetchStates( nextProps.fields.countryCode.value );
+
+			if ( this.props.fields.countryCode.value ) {
+				// Reset the state field every time the user selects a
+				// different country, but only if a country was previously
+				// selected
+				this.props.fields.state.onChange( '' );
+			}
+		}
+	}
+
+	initializeContactInformation( props = this.props ) {
+		const form = Object.keys( props.fields ).reduce( ( result, fieldName ) => {
+			let value = props.fields[ fieldName ].value || props.contactInformation.data[ fieldName ] || '';
+
+			if ( fieldName === 'phone' && props.contactInformation.data.phone ) {
+				const existingPhoneValue = props.fields.phone.value;
+				const newPhoneValue = props.contactInformation.data.phone;
+
+				if ( ! existingPhoneValue || isCallingCode( parseInt( existingPhoneValue ) ) ) {
+					// we should overwrite this field if it contains only a
+					// calling code or is empty
+					value = newPhoneValue;
+				}
+			}
+
+			// If we didn't got an email from the contactInformation above,
+			// lets set it to logged in user's email
+			if ( fieldName === 'email' && ! value ) {
+				value = props.initialEmail;
+			}
+
+			return Object.assign( result, { [ fieldName ]: value } );
+		}, {} );
+
+		props.initializeForm( form );
+	}
+
+	setCountryCode( props = this.props ) {
+		let countryCode;
+
+		// Use the GEO location
+		if ( props.userLocation.hasLoadedFromServer ) {
+			countryCode = props.userLocation.data.countryCode;
+		}
+
+		if ( props.contactInformation.hasLoadedFromServer && props.contactInformation.data.countryCode ) {
+			// Over-ride the GEO location if the user has contact information.
+			countryCode = props.contactInformation.data.countryCode;
+		}
+
+		if ( countryCode ) {
+			props.fields.countryCode.onChange( countryCode );
+		}
+	}
+
+	canUpdateCountryFromLocation( props = this.props ) {
+		return ! this.isDataLoading( props ) &&
+			( props.userLocation.hasLoadedFromServer || props.userLocation.hasFailedToLoad );
+	}
+
+	shouldFetchStates( nextProps ) {
+		return this.props.fields.countryCode.value !== nextProps.fields.countryCode.value && nextProps.fields.countryCode.value !== '';
+	}
+
+	isDataLoading( props = this.props ) {
+		return ! props.contactInformation.hasLoadedFromServer;
+	}
+
+	isSubmitButtonDisabled() {
+		const { asyncValidating, invalid, submitting } = this.props;
+
+		return asyncValidating || invalid || submitting || this.isDataLoading();
+	}
+
+	address2InputIsVisible() {
+		const { inputVisibility: { address2InputIsVisible }, fields: { address2 } } = this.props;
+
+		return address2InputIsVisible || address2.initialValue;
+	}
+
+	organizationInputIsVisible() {
+		const { inputVisibility: { organizationInputIsVisible }, fields: { organization } } = this.props;
+
+		return organizationInputIsVisible || organization.initialValue;
+	}
+
+	getContactInformation() {
+		return Object.keys( this.props.fields ).reduce( ( result, fieldName ) => {
+			return Object.assign( result, { [ fieldName ]: this.props.fields[ fieldName ].value } );
+		}, {} );
+	}
+
+	getSubmitButtonText() {
+		const { asyncValidating, submitting, ctaLabel, ctaSubmittingLabel } = this.props;
+
+		if ( asyncValidating ) {
+			return i18n.translate( 'Checking your details…' );
+		}
+
+		if ( submitting ) {
+			return ctaSubmittingLabel;
+		}
+
+		return ctaLabel;
+	}
+
+	render() {
+		const { fields, handleSubmit, untouch } = this.props;
+
+		return (
+			<Form
+				onSubmit={ handleSubmit( this.props.onContactInformationSubmit ) }
+				errors={ this.props.errors }
+				focusOnError
+			>
+				<Form.FieldArea>
+					<div>
+						<fieldset>
+							<label>{ i18n.translate( 'First Name' ) }</label>
+							<Input
+								disabled={ this.isDataLoading() }
+								field={ fields.firstName }
+								autoFocus
+								untouch={ untouch }
+								className={ styles.firstName }
+								placeholder={ i18n.translate( 'First Name' ) }
+								dir="ltr"
+							/>
+							<ValidationError field={ fields.firstName } />
+						</fieldset>
+
+						<fieldset>
+							<label>{ i18n.translate( 'Last Name' ) }</label>
+							<Input
+								disabled={ this.isDataLoading() }
+								field={ fields.lastName }
+								untouch={ untouch }
+								className={ styles.lastName }
+								placeholder={ i18n.translate( 'Last Name' ) }
+								dir="ltr"
+							/>
+							<ValidationError field={ fields.lastName } />
+						</fieldset>
+
+						{ ! this.organizationInputIsVisible() && (
+							<a className={ styles.showOrganizationLink } onClick={ this.props.showOrganizationInput }>
+								{ i18n.translate( 'Registering for a company? Add Organization name.' ) }
+							</a>
+						) }
+
+						{ this.organizationInputIsVisible() && (
+							<fieldset>
+								<label>{ i18n.translate( 'Organization' ) }</label>
+								<Input
+									field={ fields.organization }
+									untouch={ untouch }
+									className={ styles.organization }
+									disabled={ this.isDataLoading() }
+									placeholder={ i18n.translate( 'Organization' ) }
+									dir="ltr"
+								/>
+								<ValidationError field={ fields.organization } />
+							</fieldset>
+						) }
+
+						<fieldset>
+							<label>{ i18n.translate( 'Email' ) }</label>
+							<Input
+								disabled={ this.isDataLoading() }
+								field={ fields.email }
+								placeholder={ i18n.translate( 'Email' ) }
+								untouch={ untouch }
+								type="email"
+								dir="ltr"
+							/>
+							<ValidationError field={ fields.email } />
+						</fieldset>
+
+						<fieldset>
+							<label>{ i18n.translate( 'Address' ) }</label>
+
+							<Input
+								field={ fields.address1 }
+								untouch={ untouch }
+								className={ styles.address1 }
+								disabled={ this.isDataLoading() }
+								placeholder={ i18n.translate( 'Address Line 1' ) }
+								dir="ltr"
+							/>
+
+							{ this.address2InputIsVisible() && (
+								<Input
+									field={ fields.address2 }
+									untouch={ untouch }
+									className={ styles.address2 }
+									disabled={ this.isDataLoading() }
+									placeholder={ i18n.translate( 'Address Line 2' ) }
+									dir="ltr"
+								/>
+							) }
+
+							<ValidationError fields={ [ fields.address1, fields.address2 ] } />
+
+							{ ! this.address2InputIsVisible() && (
+								<a className={ styles.showAddressTwoLink } onClick={ this.props.showAddress2Input }>
+									{ i18n.translate( 'Add second line for address.' ) }
+								</a>
+							) }
+						</fieldset>
+
+						<fieldset>
+							<label>{ i18n.translate( 'Country' ) }</label>
+							<Country
+								field={ fields.countryCode }
+								className={ styles.countryCode }
+								supportedBy="domains"
+								dir="ltr"
+							/>
+							<ValidationError field={ fields.countryCode } />
+						</fieldset>
+
+						<fieldset>
+							<label>{ i18n.translate( 'City' ) }</label>
+							<Input
+								disabled={ this.isDataLoading() }
+								untouch={ untouch }
+								field={ fields.city }
+								className={ styles.city }
+								placeholder={ i18n.translate( 'City' ) }
+								dir="ltr"
+							/>
+							<ValidationError field={ fields.city } />
+						</fieldset>
+
+						<fieldset>
+							<label>{ i18n.translate( 'State/Province' ) }</label>
+							<State
+								disabled={ this.isDataLoading() }
+								field={ fields.state }
+								untouch={ untouch }
+								className={ styles.state }
+								states={ this.props.states }
+								dir="ltr"
+							/>
+							<ValidationError field={ fields.state } />
+						</fieldset>
+
+						<fieldset>
+							<label>{ i18n.translate( 'Postal Code' ) }</label>
+							<Input
+								disabled={ this.isDataLoading() }
+								untouch={ untouch }
+								field={ fields.postalCode }
+								className={ styles.postalCode }
+								placeholder={ i18n.translate( 'Postal Code' ) }
+								dir="ltr"
+							/>
+							<ValidationError field={ fields.postalCode } />
+						</fieldset>
+
+						<fieldset>
+							<label>{ i18n.translate( 'Phone' ) }</label>
+							<Phone
+								countryCode={ fields.countryCode.value }
+								disabled={ this.isDataLoading() }
+								field={ fields.phone }
+								untouch={ untouch }
+								className={ styles.phone }
+							/>
+							<ValidationError field={ fields.phone } />
+						</fieldset>
+					</div>
+				</Form.FieldArea>
+
+				<Form.SubmitArea>
+					<Button disabled={ this.isSubmitButtonDisabled() }>
+						{ this.getSubmitButtonText() }
+					</Button>
+				</Form.SubmitArea>
+
+				<Form.Footer>
+					<p>
+						{ typeof this.props.getFooterContents === 'function' && this.props.getFooterContents() }
+					</p>
+				</Form.Footer>
+			</Form>
+		);
+	}
+}
+
+ContactInformationForm.propTypes = {
+	asyncValidating: PropTypes.oneOfType( [
+		PropTypes.bool,
+		PropTypes.string,
+	] ),
+	contactInformation: PropTypes.object.isRequired,
+	ctaLabel: PropTypes.string.isRequired,
+	ctaSubmittingLabel: PropTypes.string.isRequired,
+	errors: PropTypes.object,
+	fetchContactInformation: PropTypes.func.isRequired,
+	fetchLocation: PropTypes.func.isRequired,
+	fetchStates: PropTypes.func.isRequired,
+	fields: PropTypes.object.isRequired,
+	getFooterContents: PropTypes.func,
+	handleSubmit: PropTypes.func.isRequired,
+	initialEmail: PropTypes.string.isRequired,
+	inputVisibility: PropTypes.object.isRequired,
+	invalid: PropTypes.bool.isRequired,
+	onContactInformationSubmit: PropTypes.func.isRequired,
+	resetInputVisibility: PropTypes.func.isRequired,
+	showAddress2Input: PropTypes.func.isRequired,
+	showOrganizationInput: PropTypes.func.isRequired,
+	states: PropTypes.object.isRequired,
+	submitting: PropTypes.bool.isRequired,
+	untouch: PropTypes.func.isRequired,
+	userLocation: PropTypes.object.isRequired,
+};
+
+export default withStyles( styles )( bindHandlers( ContactInformationForm ) );
--- app/components/ui/contact-information/index.js
+++ app/components/ui/contact-information/index.js
@@ -1,23 +1,17 @@
 // External dependencies
 import i18n from 'i18n-calypso';
-import isEmpty from 'lodash/isEmpty';
 import React, { PropTypes } from 'react';
 import withStyles from 'isomorphic-style-loader/lib/withStyles';
 import { bindHandlers } from 'react-bind-handlers';
 
 // Internal dependencies
-import Button from 'components/ui/button';
+import ContactInformationFormFactory from 'components/containers/contact-information-form';
 import CheckoutProgressbar from 'components/ui/checkout-progressbar';
-import Country from 'components/containers/country';
 import DocumentTitle from 'components/ui/document-title';
-import Form from 'components/ui/form';
-import Input from 'components/ui/form/input';
-import { isCallingCode } from 'lib/form';
-import Phone from 'components/ui/form/phone';
-import State from 'components/ui/form/state';
 import styles from './styles.scss';
-import ValidationError from 'components/ui/form/validation-error';
 import withPageView from 'lib/analytics/with-page-view';
+
+const ContactInformationForm = ContactInformationFormFactory( 'contactInformation' );
 
 class ContactInformation extends React.Component {
 	componentWillMount() {
@@ -25,171 +19,31 @@ class ContactInformation extends React.Component {
 			this.props.redirectToHome();
 			return;
 		}
-
-		if ( ! this.props.userLocation.isRequesting && ! this.props.userLocation.hasLoadedFromServer ) {
-			this.props.fetchLocation();
-		}
-
-		if ( ! this.props.contactInformation.isRequesting && ! this.props.contactInformation.hasLoadedFromServer ) {
-			this.props.fetchContactInformation();
-		}
-
-		if ( this.props.contactInformation.hasLoadedFromServer ) {
-			this.initializeContactInformation();
-		}
-
-		if ( this.canUpdateCountryFromLocation() ) {
-			this.setCountryCode();
-		}
-
-		this.props.resetInputVisibility();
 	}
 
-	componentWillReceiveProps( nextProps ) {
-		if ( this.isDataLoading() && ! this.isDataLoading( nextProps ) ) {
-			this.initializeContactInformation( nextProps );
-		}
-
-		if ( ! this.canUpdateCountryFromLocation() && this.canUpdateCountryFromLocation( nextProps ) ) {
-			this.setCountryCode( nextProps );
-		}
-
-		if ( this.shouldFetchStates( nextProps ) ) {
-			this.props.fetchStates( nextProps.fields.countryCode.value );
-
-			if ( this.props.fields.countryCode.value ) {
-				// Reset the state field every time the user selects a
-				// different country, but only if a country was previously
-				// selected
-				this.props.fields.state.onChange( '' );
-			}
-		}
-	}
-
-	initializeContactInformation( props = this.props ) {
-		const form = Object.keys( props.fields ).reduce( ( result, fieldName ) => {
-			let value = props.fields[ fieldName ].value || props.contactInformation.data[ fieldName ] || '';
-
-			if ( fieldName === 'phone' && props.contactInformation.data.phone ) {
-				const existingPhoneValue = props.fields.phone.value;
-				const newPhoneValue = props.contactInformation.data.phone;
-
-				if ( ! existingPhoneValue || isCallingCode( parseInt( existingPhoneValue ) ) ) {
-					// we should overwrite this field if it contains only a
-					// calling code or is empty
-					value = newPhoneValue;
-				}
-			}
-
-			// If we didn't got an email from the contactInformation above,
-			// lets set it to logged in user's email
-			if ( fieldName === 'email' && ! value ) {
-				value = props.user.data.email;
-			}
-
-			return Object.assign( result, { [ fieldName ]: value } );
-		}, {} );
-
-		props.initializeForm( form );
-	}
-
-	setCountryCode( props = this.props ) {
-		let countryCode;
-
-		// Use the GEO location
-		if ( props.userLocation.hasLoadedFromServer ) {
-			countryCode = props.userLocation.data.countryCode;
-		}
-
-		if ( props.contactInformation.hasLoadedFromServer && props.contactInformation.data.countryCode ) {
-			// Over-ride the GEO location if the user has contact information.
-			countryCode = props.contactInformation.data.countryCode;
-		}
-
-		if ( countryCode ) {
-			props.fields.countryCode.onChange( countryCode );
-		}
-	}
-
-	canUpdateCountryFromLocation( props = this.props ) {
-		return ! this.isDataLoading( props ) &&
-			( props.userLocation.hasLoadedFromServer || props.userLocation.hasFailedToLoad );
-	}
-
-	shouldFetchStates( nextProps ) {
-		return this.props.fields.countryCode.value !== nextProps.fields.countryCode.value && nextProps.fields.countryCode.value !== '';
-	}
-
-	isDataLoading( props = this.props ) {
-		return ! props.contactInformation.hasLoadedFromServer;
-	}
-
-	isSubmitButtonDisabled() {
-		const { asyncValidating, invalid, submitting } = this.props;
-
-		return asyncValidating || invalid || submitting || this.isDataLoading();
-	}
-
-	address2InputIsVisible() {
-		const { inputVisibility: { address2InputIsVisible }, fields: { address2 } } = this.props;
-
-		return address2InputIsVisible || address2.initialValue;
-	}
-
-	organizationInputIsVisible() {
-		const { inputVisibility: { organizationInputIsVisible }, fields: { organization } } = this.props;
-
-		return organizationInputIsVisible || organization.initialValue;
-	}
-
-	validate() {
-		const contactInformation = Object.keys( this.props.fields ).reduce( ( result, fieldName ) => {
-			return Object.assign( result, { [ fieldName ]: this.props.fields[ fieldName ].value } );
-		}, {} );
-
-		return this.props.validateContactInformation(
-			this.props.domain.domainName,
-			contactInformation
-		);
-	}
-
-	handleSubmission() {
-		this.validate().then( () => {
-			const { errors } = this.props;
-
-			if ( isEmpty( errors ) ) {
-				this.props.redirectToCheckout();
-			}
-		} );
-	}
-
-	getSubmitButtonText() {
-		const { asyncValidating, submitting } = this.props;
-
-		if ( asyncValidating ) {
-			return i18n.translate( 'Checking your details…' );
-		}
-
-		if ( submitting ) {
-			return i18n.translate( 'Taking you to checkout…' );
-		}
-
-		return i18n.translate( 'Continue to checkout' );
-	}
-
-	getPrivacyNoticeText() {
+	handlePrivacyNoticeText() {
 		const { hasTrademarkClaim } = this.props;
 
 		if ( hasTrademarkClaim ) {
-			return i18n.translate( 'Your contact information will be available in a public database of domain owners, called "Whois".' );
+			return i18n.translate( 'Your contact information will be available in a public' +
+				' database of domain owners, called "Whois".' );
 		}
 
-		return i18n.translate( "Domain owners are required to share contact information publicly. We keep your personal information out of your domain's public records, to protect your identity and prevent spam." );
+		return i18n.translate( 'Domain owners are required to share contact information publicly. ' +
+			"We keep your personal information out of your domain's public records," +
+			' to protect your identity and prevent spam.' );
+	}
+
+	handleFormValidation( values ) {
+		const { domain: { domainName } } = this.props;
+		return this.props.validateContactInformation( domainName, values );
+	}
+
+	handleFormSubmission() {
+		this.props.redirectToCheckout();
 	}
 
 	render() {
-		const { fields, handleSubmit, untouch } = this.props;
-
 		return (
 			<DocumentTitle title={ i18n.translate( 'Contact Information' ) }>
 				<div>
@@ -210,184 +64,13 @@ class ContactInformation extends React.Component {
 						</h3>
 					</div>
 
-					<Form
-						onSubmit={ handleSubmit( this.handleSubmission ) }
-						errors={ this.props.errors }
-						focusOnError
-					>
-						<Form.FieldArea>
-							<div>
-								<fieldset>
-									<label>{ i18n.translate( 'First Name' ) }</label>
-									<Input
-										disabled={ this.isDataLoading() }
-										field={ fields.firstName }
-										autoFocus
-										untouch={ untouch }
-										className={ styles.firstName }
-										placeholder={ i18n.translate( 'First Name' ) }
-										dir="ltr"
-									/>
-									<ValidationError field={ fields.firstName } />
-								</fieldset>
-
-								<fieldset>
-									<label>{ i18n.translate( 'Last Name' ) }</label>
-									<Input
-										disabled={ this.isDataLoading() }
-										field={ fields.lastName }
-										untouch={ untouch }
-										className={ styles.lastName }
-										placeholder={ i18n.translate( 'Last Name' ) }
-										dir="ltr"
-									/>
-									<ValidationError field={ fields.lastName } />
-								</fieldset>
-
-								{ ! this.organizationInputIsVisible() && (
-									<a className={ styles.showOrganizationLink } onClick={ this.props.showOrganizationInput }>
-										{ i18n.translate( 'Registering for a company? Add Organization name.' ) }
-									</a>
-								) }
-
-								{ this.organizationInputIsVisible() && (
-									<fieldset>
-										<label>{ i18n.translate( 'Organization' ) }</label>
-										<Input
-											autoFocus
-											field={ fields.organization }
-											untouch={ untouch }
-											className={ styles.organization }
-											disabled={ this.isDataLoading() }
-											placeholder={ i18n.translate( 'Organization' ) }
-											dir="ltr"
-										/>
-										<ValidationError field={ fields.organization } />
-									</fieldset>
-								) }
-
-								<fieldset>
-									<label>{ i18n.translate( 'Email' ) }</label>
-									<Input
-										disabled={ this.isDataLoading() }
-										field={ fields.email }
-										placeholder={ i18n.translate( 'Email' ) }
-										untouch={ untouch }
-										type="email"
-										dir="ltr"
-									/>
-									<ValidationError field={ fields.email } />
-								</fieldset>
-
-								<fieldset>
-									<label>{ i18n.translate( 'Address' ) }</label>
-
-									<Input
-										field={ fields.address1 }
-										untouch={ untouch }
-										className={ styles.address1 }
-										disabled={ this.isDataLoading() }
-										placeholder={ i18n.translate( 'Address Line 1' ) }
-										dir="ltr"
-									/>
-
-									{ this.address2InputIsVisible() && (
-										<Input
-											autoFocus
-											field={ fields.address2 }
-											untouch={ untouch }
-											className={ styles.address2 }
-											disabled={ this.isDataLoading() }
-											placeholder={ i18n.translate( 'Address Line 2' ) }
-											dir="ltr"
-										/>
-									) }
-
-									<ValidationError fields={ [ fields.address1, fields.address2 ] } />
-
-									{ ! this.address2InputIsVisible() && (
-										<a className={ styles.showAddressTwoLink } onClick={ this.props.showAddress2Input }>
-											{ i18n.translate( 'Add second line for address.' ) }
-										</a>
-									) }
-								</fieldset>
-
-								<fieldset>
-									<label>{ i18n.translate( 'Country' ) }</label>
-									<Country
-										field={ fields.countryCode }
-										className={ styles.countryCode }
-										supportedBy="domains"
-										dir="ltr"
-									/>
-									<ValidationError field={ fields.countryCode } />
-								</fieldset>
-
-								<fieldset>
-									<label>{ i18n.translate( 'City' ) }</label>
-									<Input
-										disabled={ this.isDataLoading() }
-										untouch={ untouch }
-										field={ fields.city }
-										className={ styles.city }
-										placeholder={ i18n.translate( 'City' ) }
-										dir="ltr"
-									/>
-									<ValidationError field={ fields.city } />
-								</fieldset>
-
-								<fieldset>
-									<label>{ i18n.translate( 'State/Province' ) }</label>
-									<State
-										disabled={ this.isDataLoading() }
-										field={ fields.state }
-										untouch={ untouch }
-										className={ styles.state }
-										states={ this.props.states }
-										dir="ltr"
-									/>
-									<ValidationError field={ fields.state } />
-								</fieldset>
-
-								<fieldset>
-									<label>{ i18n.translate( 'Postal Code' ) }</label>
-									<Input
-										disabled={ this.isDataLoading() }
-										untouch={ untouch }
-										field={ fields.postalCode }
-										className={ styles.postalCode }
-										placeholder={ i18n.translate( 'Postal Code' ) }
-										dir="ltr"
-									/>
-									<ValidationError field={ fields.postalCode } />
-								</fieldset>
-
-								<fieldset>
-									<label>{ i18n.translate( 'Phone' ) }</label>
-									<Phone
-										countryCode={ fields.countryCode.value }
-										disabled={ this.isDataLoading() }
-										field={ fields.phone }
-										untouch={ untouch }
-										className={ styles.phone }
-									/>
-									<ValidationError field={ fields.phone } />
-								</fieldset>
-							</div>
-						</Form.FieldArea>
-
-						<Form.SubmitArea>
-							<Button disabled={ this.isSubmitButtonDisabled() }>
-								{ this.getSubmitButtonText() }
-							</Button>
-						</Form.SubmitArea>
-
-						<Form.Footer>
-							<p>
-								{ this.getPrivacyNoticeText() }
-							</p>
-						</Form.Footer>
-					</Form>
+					<ContactInformationForm
+						contactInformationValidationFunction={ this.handleFormValidation }
+						onContactInformationSubmit={ this.handleFormSubmission }
+						ctaLabel={ i18n.translate( 'Continue to checkout' ) }
+						ctaSubmittingLabel={ i18n.translate( 'Taking you to checkout…' ) }
+						getFooterContents={ this.handlePrivacyNoticeText }
+					/>
 				</div>
 			</DocumentTitle>
 		);
@@ -402,24 +85,11 @@ ContactInformation.propTypes = {
 	contactInformation: PropTypes.object.isRequired,
 	domain: PropTypes.object,
 	errors: PropTypes.object,
-	fetchContactInformation: PropTypes.func.isRequired,
-	fetchLocation: PropTypes.func.isRequired,
-	fetchStates: PropTypes.func.isRequired,
-	fields: PropTypes.object.isRequired,
 	handleSubmit: PropTypes.func.isRequired,
 	hasSelectedDomain: PropTypes.bool.isRequired,
 	hasTrademarkClaim: PropTypes.bool.isRequired,
-	inputVisibility: PropTypes.object.isRequired,
-	invalid: PropTypes.bool.isRequired,
 	redirectToCheckout: PropTypes.func.isRequired,
 	redirectToHome: PropTypes.func.isRequired,
-	resetInputVisibility: PropTypes.func.isRequired,
-	showAddress2Input: PropTypes.func.isRequired,
-	showOrganizationInput: PropTypes.func.isRequired,
-	states: PropTypes.object.isRequired,
-	submitting: PropTypes.bool.isRequired,
-	untouch: PropTypes.func.isRequired,
-	user: PropTypes.object.isRequired,
 	userLocation: PropTypes.object.isRequired,
 	validateContactInformation: PropTypes.func.isRequired
 };

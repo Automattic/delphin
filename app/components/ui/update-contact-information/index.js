ebdc776df4c3148dd437ec5a4e47c212320c60a7
Yury Michurin <yury@nix.co.il>
2017-01-09 15:31:24 +0200

    Change method name to a clearer one

--- app/components/ui/update-contact-information/index.js
+++ app/components/ui/update-contact-information/index.js
@@ -19,7 +19,7 @@ class UpdateContactInformation extends React.Component {
 		}
 	}
 
-	areDomainsReady() {
+	isDataLoading() {
 		return this.props.domains.isRequesting || ! this.props.domains.hasLoadedFromServer;
 	}
 
@@ -63,7 +63,7 @@ class UpdateContactInformation extends React.Component {
 	getAffectedDomainsNotice() {
 		return <div>
 			<h4 className={ styles.text }>{ i18n.translate( 'Changes will apply to the following domains:' ) }</h4>
-			{ this.areDomainsReady()
+			{ this.isDataLoading()
 				? i18n.translate( 'Loading your domains…' )
 				: <ul>
 				{
@@ -100,7 +100,7 @@ class UpdateContactInformation extends React.Component {
 						submitButtonSubmittingLabel={ i18n.translate( 'Updating WHOIS data…' ) }
 						preSubmitContent={ this.getAffectedDomainsNotice() }
 						footerContent={ this.getWhoisPrivacyNotice() }
-						submitDisabled={ this.areDomainsReady() }
+						submitDisabled={ this.isDataLoading() }
 					/>
 				</div>
 			</DocumentTitle>
